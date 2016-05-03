3d54d233ec3cbff596e1db21fc3a1b96a1a3775b
Tobias Binder <Tobias.Binder@awi.de>
2016-05-03 13:27:30 +0000

    Added chan1 to all folders, better comment for pdf plot

--- cresis-toolbox/missions/run_basic_noise_analysis.m
+++ cresis-toolbox/missions/run_basic_noise_analysis.m
@@ -20,9 +20,9 @@ if strcmpi(radar_setup,'MCORDS5')
   param.file_search_mode = 'last_file';
 
   % .base_dir_search: cell vector of paths to search for data files
-  param.base_dir_search = {'D:\awi\chan1','/mnt/HDD6/1604130301/UWB/','/mnt/AWI_SSD0/1604180702/UWB'};
+  param.base_dir_search = {'D:\awi\chan1','/mnt/HDD6/1604130301/UWB/chan1/','/mnt/AWI_SSD0/1604180702/UWB/chan1/'};
   
-  % .pdf_en: Enable pdf plot
+  % .pdf_en: Enable time domain, probability density function, and quantization plots
   param.pdf_en = false;
   % .psd_en: Enable PSD plot
   param.psd_en = true;
