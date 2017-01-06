32ff3022ddbc596bd0fb5535b161016a366b4ea9
John Paden <paden@ku.edu>
2017-01-06 11:18:58 -0600

    insar: Added medium and bad lines 2016 Greenland G1XB, improved insar plots

--- cresis-toolbox/+insar/insar.m
+++ cresis-toolbox/+insar/insar.m
@@ -1,9 +1,22 @@
 
-if ispc
-  fn = 'X:/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/good_line.mat';
-  fn = 'E:\rds\2016_Greenland_G1XB\CSARP_insar\good_line.mat';
+if 1
+  if ispc
+    fn = 'X:/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/good_line.mat';
+  else
+    fn = '/cresis/snfs1/dataproducts/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/good_line.mat';
+  end
+elseif 0
+  if ispc
+    fn = 'X:/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/medium_line.mat';
+  else
+    fn = '/cresis/snfs1/dataproducts/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/medium_line.mat';
+  end
 else
-  fn = '/cresis/snfs1/dataproducts/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/good_line.mat';
+  if ispc
+    fn = 'X:/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/bad_line.mat';
+  else
+    fn = '/cresis/snfs1/dataproducts/ct_data/rds/2016_Greenland_G1XB/CSARP_insar/bad_line.mat';
+  end
 end
 
 proj = geotiffinfo(ct_filename_gis([],fullfile('greenland','Landsat-7','Greenland_natural_90m.tif')));
@@ -11,15 +24,19 @@ proj = geotiffinfo(ct_filename_gis([],fullfile('greenland','Landsat-7','Greenlan
 physical_constants;
 
 load(fn);
-master_idx = 6;
+master_idx = 2;
 rbins = 20:120;
 
 %% Plot Results
 % =========================================================================
 h_fig_map = figure(100); clf;
+h_plot_map = [];
+h_legend_map = {};
 hold on;
 axis('equal');
 h_fig_elev = figure(101); clf;
+h_plot_elev = [];
+h_legend_elev = {};
 hold on;
 xlabel('Range line');
 ylabel('WGS-84 elevation (m)');
@@ -52,8 +69,9 @@ for pass_idx = 1:length(pass)
     h_text = text(pass(pass_idx).lon(1), pass(pass_idx).lat(1), sprintf('%d', pass_idx), 'Color', color);
   else
     [pass(pass_idx).proj_x,pass(pass_idx).proj_y] = projfwd(proj,pass(pass_idx).lat,pass(pass_idx).lon);
-    h_plot = plot(pass(pass_idx).proj_x/1e3, pass(pass_idx).proj_y/1e3,'.');
-    color = get(h_plot,'Color');
+    h_plot_map(end+1) = plot(pass(pass_idx).proj_x/1e3, pass(pass_idx).proj_y/1e3,'.');
+    h_legend_map{end+1} = sprintf('%d',pass_idx);
+    color = get(h_plot_map(end),'Color');
     h_text = text(pass(pass_idx).proj_x(1)/1e3, pass(pass_idx).proj_y(1)/1e3, sprintf('%d', pass_idx), 'Color', color);
     xlabel('X (km)');
     ylabel('Y (km)');
@@ -62,16 +80,18 @@ for pass_idx = 1:length(pass)
   
   figure(h_fig_elev);
   if master_idx == pass_idx
-    plot(pass(pass_idx).elev,'LineWidth',2);
+    h_plot_elev(end+1) = plot(pass(pass_idx).elev,'LineWidth',2);
   else
-    plot(pass(pass_idx).elev);
+    h_plot_elev(end+1) = plot(pass(pass_idx).elev);
   end
+  h_legend_elev{end+1} = sprintf('%d',pass_idx');
   
   pass(pass_idx).ecef = [];
   [pass(pass_idx).ecef(1,:),pass(pass_idx).ecef(2,:),pass(pass_idx).ecef(3,:)] ...
     = geodetic2ecef(pass(pass_idx).lat/180*pi, pass(pass_idx).lon/180*pi, pass(pass_idx).elev, WGS84.ellipsoid);
 end
 linkaxes(h_data_axes,'xy');
+legend(h_plot_map,h_legend_map);
 
 %% Co-Register Results
 % =========================================================================
@@ -94,8 +114,10 @@ end
 along_track = geodetic_to_along_track(ref.lat,ref.lon,ref.elev);
 ref.surface_bin = interp1(ref.wfs.time, 1:length(ref.wfs.time), ref.surface);
 
-h_fig_ref_idx = figure(102); clf;
-hold on;
+if 0
+  h_fig_ref_idx = figure(102); clf;
+  hold on;
+end
 
 h_data_axes = [];
 data = [];
@@ -156,7 +178,7 @@ for pass_idx = 1:length(pass)
 
   if 0
     % Motion compensation of FCS z-motion
-    for rline = 1:size(pass(pass_idx).ecef,2)
+    for rline = 1:size(pass(pass_idx).ref_data,2)
       % Convert z-offset into time-offset assuming nadir DOA
       dt = pass(pass_idx).ref_z(rline)/(c/2);
       pass(pass_idx).ref_data(:,rline) = ifft(fft(pass(pass_idx).ref_data(:,rline)) ...
@@ -203,78 +225,117 @@ data = {permute(data,[1 2 4 5 3])};
 array_param.method = 1;
 array_param.Nsv = 64;
 array_param.Nsig = 2;
-array_param.bin_rng = [-1:1];
+array_param.bin_rng = [0];
 array_param.rline_rng = [-21:21];
 array_param.dbin = 1;
 array_param.dline = 1;
 array_param.freq_rng = 1;
-figure(200); clf;
+h_fig_baseline = figure(200); clf;
+h_plot_baseline = [];
+h_legend_baseline = {};
 for pass_idx = 1:length(pass)
+  array_param.fcs{1}{pass_idx}.pos = along_track;
   array_param.fcs{1}{pass_idx}.pos(2,:) = pass(pass_idx).ref_y;
   array_param.fcs{1}{pass_idx}.pos(3,:) = pass(pass_idx).ref_z;
-  array_param.fcs{1}{pass_idx}.base_line ...
+  array_param.fcs{1}{pass_idx}.base_line ...        
     = sqrt( (pass(pass_idx).ref_z - pass(master_idx).ref_z).^2 ...
       + (pass(pass_idx).ref_y - pass(master_idx).ref_y).^2 );
     
-  plot(array_param.fcs{1}{pass_idx}.base_line);
+  h_plot_baseline(end+1) = plot(array_param.fcs{1}{pass_idx}.base_line);
+  h_legend_baseline{end+1} = sprintf('%d',pass_idx);
   hold on;
 
-  array_param.fcs{1}{pass_idx}.surface(:) = ref.surface;
+  array_param.fcs{1}{pass_idx}.surface = ref.surface;
 end
 xlabel('Range line');
 ylabel('Baseline (m)');
 grid on;
+legend(h_plot_baseline,h_legend_baseline);
 
 array_param.wfs.time = ref.wfs.time;
 array_param.sv_fh = @array_proc_sv;
 array_param.wfs.fc = pass(1).wfs.fc;
-array_param.imgs = {[ones(6,1), (1:6).']};
+array_param.imgs = {[ones(length(pass),1), (1:length(pass)).']};
 array_param.three_dim.en = true;
 
 
 %%
 array_param.method = 0;
-[array_param2,result0] = array_proc(array_param,data);
+[array_param0,result0] = array_proc(array_param,data);
 array_param.method = 1;
-[array_param2,result1] = array_proc(array_param,data);
+[array_param1,result1] = array_proc(array_param,data);
 array_param.method = 2;
 [array_param2,result2] = array_proc(array_param,data);
 
-figure(1); clf;
+figure(11); clf;
 imagesc(lp(result0.val))
 colormap(1-gray(256))
 h_axes = gca;
 
-figure(2); clf;
+figure(12); clf;
 imagesc(lp(result1.val))
 colormap(1-gray(256))
 h_axes(end+1) = gca;
 
-figure(3); clf;
+figure(13); clf;
 imagesc(lp(result2.val))
 colormap(1-gray(256))
 h_axes(end+1) = gca;
 
-figure(4); clf;
-imagesc(lp(data{1}(:,:,master_idx)))
+figure(14); clf;
+imagesc(lp(fir_dec(abs(data{1}(:,:,master_idx)).^2, ones(size(array_param.rline_rng)), 1, ...
+  1-array_param.rline_rng(1), size(data{1}(:,:,master_idx),2)-length(array_param.rline_rng)+1)))
 colormap(1-gray(256))
-h_axes = gca;
+h_axes(end+1) = gca;
 
 linkaxes(h_axes,'xy');
 
-return
-%%
+%% Save Results
+[fn_dir,fn_name] = fileparts(fn);
+fn_map = fullfile(fn_dir,[fn_name '_map.fig']);
+saveas(h_fig_map,fn_map);
+fn_elev = fullfile(fn_dir,[fn_name '_elev.fig']);
+saveas(h_fig_elev,fn_elev);
+fn_baseline = fullfile(fn_dir,[fn_name '_baseline.fig']);
+saveas(h_fig_baseline,fn_baseline);
+fn_standard = fullfile(fn_dir,[fn_name '_standard.fig']);
+saveas(1,fn_standard);
+fn_mvdr = fullfile(fn_dir,[fn_name '_mvdr.fig']);
+saveas(2,fn_mvdr);
+fn_music = fullfile(fn_dir,[fn_name '_music.fig']);
+saveas(3,fn_music);
+fn_single = fullfile(fn_dir,[fn_name '_single.fig']);
+saveas(4,fn_single);
 
-figure(5); clf;
-for idx = 400:600%1:50:size(result1.img,3)
-  imagesc(lp(result1.img(:,:,idx)))
+return
+
+
+
+%% Other plots and plot setup
+
+
+figure(500); clf;
+for idx = 1800:-1:600%1:50:size(result1.img,3)
+  imagesc(lp(result0.img(20:120,:,idx)))
   colormap(jet(256));
+  caxis([-80 -20])
   idx
   pause
 end
 
+%%
 
-%% Other plots and plot setup
+figure; plot(array_param2.theta*180/pi,lp(result1.img(20+54-1,:,1291)))
+grid on
+xlim([-90 90])
+xlabel('Direction of arrival (deg)');
+ylabel('Relative power (dB)');
+title('Bad line, MVDR passes [2 4 6], rline 1291');
+
+>> array_param.fcs{1}{6}.pos(2:3,1291)
+ans =
+
+
 figure(h_fig_ref_idx); h_axes = gca;
 %figure(h_fig_map); h_axes(end+1) = gca;
 figure(h_fig_elev); h_axes(end+1) = gca;
