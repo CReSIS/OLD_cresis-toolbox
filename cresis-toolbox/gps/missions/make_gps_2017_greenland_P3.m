4a5892c933a2de9784b2fb81ce57ed3064e9b431
John Paden <paden@ku.edu>
2017-04-04 15:15:47 +0200

    make_gps_2017_greenland_P3.m: Field update, coh_noise_tracker.m: Fixed rerun_only filename check to handle images

--- cresis-toolbox/gps/missions/make_gps_2017_greenland_P3.m
+++ cresis-toolbox/gps/missions/make_gps_2017_greenland_P3.m
@@ -149,8 +149,17 @@ if strcmpi(gps_source_to_use,'NMEA')
 %     params{file_idx} = struct('year',year,'month',month,'day',day,'format',3,'time_reference','utc');
 %     gps_source{file_idx} = 'nmea-field';
 %     sync_flag{file_idx} = 0;
+%       
+%     year = 2017; month = 3; day = 31;
+%     file_idx = file_idx + 1;
+%     in_fns{file_idx} = get_filenames(fullfile(in_base_path,sprintf('%04d%02d%02d',year,month,day)),'GPS','','.txt');
+%     out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
+%     file_type{file_idx} = 'NMEA';
+%     params{file_idx} = struct('year',year,'month',month,'day',day,'format',3,'time_reference','utc');
+%     gps_source{file_idx} = 'nmea-field';
+%     sync_flag{file_idx} = 0;
       
-    year = 2017; month = 3; day = 31;
+    year = 2017; month = 4; day = 3;
     file_idx = file_idx + 1;
     in_fns{file_idx} = get_filenames(fullfile(in_base_path,sprintf('%04d%02d%02d',year,month,day)),'GPS','','.txt');
     out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
@@ -304,6 +313,16 @@ make_gps;
 
 % Debug code that sets up special processing
 hack_idx = cell2mat(strfind(out_fns,'gps_20170329.mat'));
+if ~isempty(hack_idx)
+  out_fn = fullfile(gps_path,out_fns{hack_idx});
+  
+  warning('Making monotonic gps time: %s', out_fn);
+  
+  gps = load(out_fn);
+  [gps,error_flag] = make_gps_monotonic(gps);
+  save(out_fn,'-append','-struct','gps');
+end
+hack_idx = cell2mat(strfind(out_fns,'gps_20170403.mat'));
 if ~isempty(hack_idx)
   out_fn = fullfile(gps_path,out_fns{hack_idx});
   
