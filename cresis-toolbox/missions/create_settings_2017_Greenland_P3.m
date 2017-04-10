731644023b90ca88294930e2d9223cec48170723
John Paden <paden@ku.edu>
2017-04-10 07:25:02 -0300

    Field updates: create_settings_2017_Greenland_P3.m, default_radar_params_2017_Greenland_P3_mcords.m

--- cresis-toolbox/missions/create_settings_2017_Greenland_P3.m
+++ cresis-toolbox/missions/create_settings_2017_Greenland_P3.m
@@ -291,6 +291,43 @@ for Tpd = [10e-6]
   write_cresis_xml(param);
 end
 
+%% Equalization Low Altitude
+% 1250 +/- 750 ft AGL
+% For lower altitude, increase attenuation
+% Use these settings over ocean or sea ice for fast-time equalization,
+% transmit equalization, and receiver equalization.
+% Creates one waveform for each of N DDS-transmitters plus a combined
+% waveform with all transmitters going.
+freq_idx = 1;
+for Tpd = [1e-6]
+  param = struct('radar_name','mcords3','num_chan',15,'aux_dac',[255 255 255 255 255 255 255 255],'version','10.0','TTL_prog_delay',650,'fs',1e9/9,'fs_sync',1e9/18,'fs_dds',1e9,'TTL_clock',1e9/18,'TTL_mode',[3e-6 290e-9 -1060e-9],'xml_version',2.0,'DDC_freq',0,'DDC_select',0);  
+  param.max_tx = [40000 40000 40000 40000 40000 40000 40000 0]; param.max_data_rate = 150; param.flight_hours = 7; param.sys_delay = 12.18e-6;
+  param.max_duty_cycle = 0.12;
+  param.create_IQ = false;
+  param.tg.altitude_guard = 6000*12*2.54/100;
+  param.tg.staged_recording = false;
+  param.tg.Haltitude = 6000*12*2.54/100;
+  param.tg.Hice_thick = 0;
+  param.fn = fullfile(base_dir,sprintf('equalization_%.0fus_%.0fft.xml', Tpd*1e6, param.tg.Haltitude*100/2.54/12));
+  param.prf = 12000;
+  param.presums = [15 15 15 15 15 15 15 15];
+  [param.wfs(1:8).atten] = deal(30);
+  param.wfs(8).atten = deal(45);
+  param.tx_weights = final_DDS_amp{cal_settings(freq_idx)};
+  param.tukey = 0.2;
+  param.Tpd = Tpd;
+  param.phase = final_DDS_phase{cal_settings(freq_idx)};
+  param.delay = final_DDS_time{cal_settings(freq_idx)};
+  param.f0 = f0_list(freq_idx);
+  param.f1 = f1_list(freq_idx);
+  for wf=1:7
+    param.wfs(wf).tx_mask = ones(1,8);
+    param.wfs(wf).tx_mask(9-wf) = 0;
+  end
+  param.wfs(8).tx_mask = zeros(1,8);
+  write_cresis_xml(param);
+end
+
 %% Equalization Lab 
 % Use these settings in the lab to demonstrate transmit equalization.
 % Creates one waveform for each of N DDS-transmitters plus a combined
