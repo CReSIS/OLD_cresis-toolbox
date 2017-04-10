c5c0ef07bc6f77d5fe8b1732d91c69dcadcc7087
John Paden <paden@ku.edu>
2017-04-10 08:08:31 -0300

    field updates: make_gps_2017_greenland_P3.m, default_radar_params_2017_Greenland_P3_mcords.m

--- cresis-toolbox/missions/default_radar_params_2017_Greenland_P3_mcords.m
+++ cresis-toolbox/missions/default_radar_params_2017_Greenland_P3_mcords.m
@@ -175,6 +175,18 @@ default.xml_regexp = 'survey_.*thick.xml';
 default.name = 'Survey Mode';
 defaults{end+1} = default;
 
+% survey mode
+default.get_heights.qlook.img_comb = [3e-06 -inf 1e-06];
+default.get_heights.imgs = {[1*ones(4,1),(2:5).'],[2*ones(4,1),(2:5).']};
+default.csarp.imgs = default.get_heights.imgs;
+default.combine.imgs = default.get_heights.imgs;
+default.combine.img_comb = default.get_heights.qlook.img_comb;
+default.radar.DC_adjust = {'','',''};
+default.radar.ref_fn = '';
+default.xml_regexp = 'survey_.*thin_ice.xml';
+default.name = 'Thin Ice Mode';
+defaults{end+1} = default;
+
 % high altitude mode
 default.get_heights.qlook.img_comb = [1e-05 -inf 3e-06];
 default.get_heights.imgs = {[1*ones(4,1),(2:5).'],[2*ones(4,1),(2:5).']};
