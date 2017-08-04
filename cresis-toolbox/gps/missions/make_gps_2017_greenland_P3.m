0eef89abff81dbdd07a67b8a3ecdd6b6412e0a32
manjish <m983a462@ku.edu>
2017-08-04 10:50:17 -0500

    make_gps_2017_greenland_P3.m: Added code for GPS file creation using ATM final

--- cresis-toolbox/gps/missions/make_gps_2017_greenland_P3.m
+++ cresis-toolbox/gps/missions/make_gps_2017_greenland_P3.m
@@ -562,15 +562,31 @@ elseif strcmpi(gps_source_to_use,'ATM')
   %   end
   % !!!   ALL data, including test flights, from Oct 7th to OCT29th are in
   % GPS time. From Nov2nd to Nov 22nd the data is in UTC time.
+ ATM_fns = get_filenames(in_base_path,'','','.out');
+  fn_dates = [];
+  for idx = 1:length(ATM_fns)
+    fn = ATM_fns{idx};
+    [~,fn_name] = fileparts(fn);
+    fn_dates(end+1) = datenum(sprintf('%s %s, 20%s', fn_name(9:11), fn_name(7:8), fn_name(12:13)));
+  end
+  fn_dates = sort(fn_dates);
   
-  year = 2017; month = 2; day = 26;
-  file_idx = file_idx + 1;
-  in_fns{file_idx} = get_filename(in_base_path,'BD982_',datestr(datenum(year,month,day),'ddmmmyy'),'PPPK*.out');
-  out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
-  file_type{file_idx} = 'applanix';
-  params{file_idx} = struct('year',year,'month',month,'day',day,'time_reference','utc');
-  gps_source{file_idx} = 'atm-final_X';
-  sync_flag{file_idx} = 0;
+
+  for idx = 1:length(fn_dates)
+    [year,month,day] = datevec(fn_dates(idx));
+    fprintf('year = %d; month = %d; day = %d;\n', year, month, day);
+    file_idx = file_idx + 1;
+      if idx==1 
+           in_fns{file_idx} = get_filename(in_base_path,'BD982_',datestr(datenum(year,month,day),'ddmmmyy'),'GNSSK*.out');
+      else  
+           in_fns{file_idx} = get_filename(in_base_path,'BD982_',datestr(datenum(year,month,day),'ddmmmyy'),'PPPK*.out');
+      end
+    out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
+    file_type{file_idx} = 'applanix';
+    params{file_idx} = struct('year',year,'month',month,'day',day,'format',3,'time_reference','utc');
+    gps_source{file_idx} = 'atm-final_20170620';
+    sync_flag{file_idx} = 0;
+  end
   
 end
 
