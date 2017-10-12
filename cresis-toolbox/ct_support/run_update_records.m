0456ad7991c48dc375a24e4b3816d3cdbc60c4a0
John Paden <paden@ku.edu>
2017-10-11 21:19:58 -0500

    run_update_records.m, update_records.m: Simplified and standardized

--- cresis-toolbox/ct_support/run_update_records.m
+++ cresis-toolbox/ct_support/run_update_records.m
@@ -7,8 +7,6 @@
 %
 % See also update_records.m
 
-clear param;
-
 % ======================================================================
 % User Settings
 % ======================================================================
@@ -16,33 +14,8 @@ clear param;
 % Parameters spreadsheet to use for updating
 %   1. Segment and frame list are taken from the parameter sheet
 %   2. For GPS update, GPS time offsets are pulled from the parameter sheet
-param.param_fn = ct_filename_param('rds_param_2009_Greenland_TO.xls');
+params = read_param_xls(ct_filename_param('kuband_param_2016_Antarctica_DC8.xls'),'20161112_03','post');
+params.cmd.generic = 1;
+% params = read_param_xls(ct_filename_param('rds_param_2009_Greenland_TO.xls'),'','post');
 
-% param.skip_phrase: All segments will be skipped with this phrase in their
-%   verification field. "do not process" is the standard. Leave this field
-%   blank to do all segments.
-param.skip_phrase = 'do not process';
-
-% param.save_changes: Logical, For debugging purposes, you can turn the file save on/off
-param.save_changes = false;
-
-% param.debug_level: default is 1, anything higher is for debugging
-param.debug_level = 1;
-
-param.layers.update_en = false;
-param.layers.source_type = 'ops'; % 'ops' or 'layerData'
-param.layers.path = ''; % Required for 'layerData'
-param.layers.layer_names = {'surface' 'bottom'}; % Required for 'ops'
-param.layers.gaps_dist = [300 60]; % Required for 'ops'
-
-param.gps_time.update_en = false;
-param.gps_time.path = '';
-param.gps_time.time_offset = -14;
-param.gps_time.special_en = false;
-
-param.gps_source.update_en = false;
-param.gps_source.path = '';
-
-update_records(param);
-
-return;
+update_records(params);
