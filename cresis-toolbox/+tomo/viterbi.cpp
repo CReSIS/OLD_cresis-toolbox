af055d28cbd8ff902e0fbb8d03c693e556aad32f
Victor Berger <victorluiz@ku.edu>
2017-12-28 11:02:06 -0600

    Minor changes to viterbi c++ programs

--- cresis-toolbox/+tomo/viterbi.cpp
+++ cresis-toolbox/+tomo/viterbi.cpp
@@ -1,166 +1,172 @@
+// viterbi.cpp
+//
+// Layer-tracking program based on the Viterbi algorithm
+// 
+// Adapted from original code by Mingze Xu, David Crandall, and John Paden
+//
+// Author: Victor Berger
+// See also: viterbi_lib.h
+
 #include "viterbi_lib.h"
 const bool debug = 0;
 
 // Entry point from Matlab
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-    
-    mexPrintf("\nVITERBI program, Dec 26/27 2017 build v1.2.1 ");
-    
-	if(nrhs != 14 && nrhs != 15)
-		mexErrMsgTxt("Usage: [labels] = viterbi(input_img, surface_gt, bottom_gt, extra_gt, ice_mask, mean, var, mid, egt_weight, smooth_weight, smooth_var, smooth_slope, bounds, viterbi_weight, repulsion, ice_bin_thr)\n");
+  if(nrhs != 14 && nrhs != 15)
+	mexErrMsgTxt("Usage: [labels] = viterbi(input_img, surface_gt, bottom_gt, extra_gt, ice_mask, mean, var, mid, egt_weight, smooth_weight, smooth_var, smooth_slope, bounds, viterbi_weight, repulsion, ice_bin_thr)\n");
 
-	if(debug)
-		mexPrintf("\n$$$ DEBUG MODE ON $$$\n");
+  if(debug)
+	mexPrintf("\n$$$ DEBUG MODE ON $$$\n");
 
-	const int    _row             = mxGetM(prhs[0]);
-	const int    _col             = mxGetN(prhs[0]);
-	const double *_in_data        = mxGetPr(prhs[0]);
-	const double *_surf_tr        = mxGetPr(prhs[1]);
-	const double *t_bott_tr       = mxGetPr(prhs[2]);
-	const double *t_extra_truth   = mxGetPr(prhs[3]);
-	const double *_mask           = mxGetPr(prhs[4]);
-	const double *_mu             = mxGetPr(prhs[5]); // mean
-	const double *_sigma          = mxGetPr(prhs[6]); // variance
-	const double *t_egt_weight    = mxGetPr(prhs[7]);
-	const double *t_smooth_weight = mxGetPr(prhs[8]);
-	const double *t_smooth_var    = mxGetPr(prhs[9]);
-	const double *_smooth_slope   = mxGetPr(prhs[10]);
-    const double *_weight_points  = mxGetPr(prhs[12]);
-    const double *t_repulsion     = mxGetPr(prhs[13]);
-    const double *t_ice_bin_thr   = mxGetPr(prhs[14]);
-	const int    _num_extra_tr    = mxGetNumberOfElements(prhs[3]);
-	const size_t _ms              = mxGetNumberOfElements(prhs[5]);
+  const int    _row             = mxGetM(prhs[0]);
+  const int    _col             = mxGetN(prhs[0]);
+  const double *_in_data        = mxGetPr(prhs[0]);
+  const double *_surf_tr        = mxGetPr(prhs[1]);
+  const double *t_bott_tr       = mxGetPr(prhs[2]);
+  const double *t_extra_truth   = mxGetPr(prhs[3]);
+  const double *_mask           = mxGetPr(prhs[4]);
+  const double *_mu             = mxGetPr(prhs[5]); // mean
+  const double *_sigma          = mxGetPr(prhs[6]); // variance
+  const double *t_egt_weight    = mxGetPr(prhs[7]);
+  const double *t_smooth_weight = mxGetPr(prhs[8]);
+  const double *t_smooth_var    = mxGetPr(prhs[9]);
+  const double *_smooth_slope   = mxGetPr(prhs[10]);
+  const double *_weight_points  = mxGetPr(prhs[12]);
+  const double *t_repulsion     = mxGetPr(prhs[13]);
+  const double *t_ice_bin_thr   = mxGetPr(prhs[14]);
+  const int    _num_extra_tr    = mxGetNumberOfElements(prhs[3]);
+  const size_t _ms              = mxGetNumberOfElements(prhs[5]);
 
-	if(_num_extra_tr % 2 != 0)
-		mexErrMsgTxt("\nERROR:\nExtra truth vector not properly defined.\n");
+  if(_num_extra_tr % 2 != 0)
+	mexErrMsgTxt("\nERROR:\nExtra truth vector not properly defined.\n");
 
-	// Bounds
-	ptrdiff_t _bounds[2];
-	if(nrhs >= 13 && mxGetNumberOfElements(prhs[11]))
-	{
-		if (!mxIsInt64(prhs[11]))
-			mexErrMsgTxt("Usage: bounds must be type int64");
-		if (mxGetNumberOfElements(prhs[11]) != 2)
-			mexErrMsgTxt("Usage: bounds must be a 2 element vector");
+  // Bounds
+  ptrdiff_t _bounds[2];
+  if(nrhs >= 13 && mxGetNumberOfElements(prhs[11]))
+  {
+    if(!mxIsInt64(prhs[11]))
+		mexErrMsgTxt("Usage: bounds must be type int64");
+    if(mxGetNumberOfElements(prhs[11]) != 2)
+		mexErrMsgTxt("Usage: bounds must be a 2 element vector");
 
-		ptrdiff_t *tmp = (ptrdiff_t*)mxGetPr(prhs[11]);
-		_bounds[0] = tmp[0];
-		_bounds[1] = tmp[1];
-		if (_bounds[0] < 0)
-			_bounds[0] = 0;
-		if (_bounds[1] < 0)
-			_bounds[1] = _col;
-		if (_bounds[0] > _col)
-			mexErrMsgTxt("Usage: bounds[0] <= size(input,2)");
-		if (_bounds[1] > _col)
-			mexErrMsgTxt("Usage: bounds[1] <= size(input,2)");  
-		if (_bounds[1] < _bounds[0])
-			mexErrMsgTxt("Usage: bounds[1] must be greater than bounds[0]");
-
-	}
-	else
-	{
-		// Default setting is to process all columns
+    ptrdiff_t *tmp = (ptrdiff_t*)mxGetPr(prhs[11]);
+    _bounds[0] = tmp[0];
+    _bounds[1] = tmp[1];
+    if(_bounds[0] < 0)
 		_bounds[0] = 0;
+    if(_bounds[1] < 0)
 		_bounds[1] = _col;
-	}
+    if(_bounds[0] > _col)
+		mexErrMsgTxt("Usage: bounds[0] <= size(input,2)");
+    if(_bounds[1] > _col)
+		mexErrMsgTxt("Usage: bounds[1] <= size(input,2)");
+    if(_bounds[1] < _bounds[0])
+		mexErrMsgTxt("Usage: bounds[1] must be greater than bounds[0]");
 
-	// Initialize surface layer array
-	int _slayer[_col];
-	for(int k = 0; k < _col; ++k)
-		_slayer[k] = (int)_surf_tr[k];
+  }
+  else
+  {
+    // Default setting is to process all columns
+    _bounds[0] = 0;
+    _bounds[1] = _col;
+  }
 
-	// Initialize variables to default values if temporary values not set
-	const int _mid              = floor(_col / 2);
-	const int _blayer           = ((t_bott_tr) ? (t_bott_tr[0] > 0 ? round(t_bott_tr[0]) : -1) : -1);
-	const double _egt_weight    = t_egt_weight && t_egt_weight < 0 ? DEF_EGT_WEIGHT : t_egt_weight[0];
-    const double _smooth_weight = t_smooth_weight[0] < 0 ? DEF_SIGMA : t_smooth_weight[0];
-	const double _smooth_var    = t_smooth_var[0] < 0 ? DEF_SIGMA : t_smooth_var[0];
-    const double _repulsion     = t_repulsion[0] < 0 ? DEF_REPULSION : t_repulsion[0];
-    const double _ice_bin_thr   = t_ice_bin_thr[0] < 0 ? DEF_ICE_BIN_THR : t_ice_bin_thr[0];
+  // Initialize surface layer array
+  int _slayer[_col];
+  for(int k = 0; k < _col; ++k)
+	_slayer[k] = (int)_surf_tr[k];
 
-	double _extra_truth_x[(_num_extra_tr / 2)],
-	       _extra_truth_y[(_num_extra_tr / 2)];
-	for (int p = 0; p < (_num_extra_tr / 2); ++p)
-	{
-		_extra_truth_x[p] = t_extra_truth[p * 2];
-		_extra_truth_y[p] = t_extra_truth[(p * 2) + 1];
-	}
+  // Initialize variables to default values if temporary values not set
+  const int _mid              = floor(_col / 2);
+  const int _blayer           = ((t_bott_tr) ? (t_bott_tr[0] > 0 ? round(t_bott_tr[0]) : -1) : -1);
+  const double _egt_weight    = t_egt_weight && t_egt_weight < 0 ? DEF_EGT_WEIGHT : t_egt_weight[0];
+  const double _smooth_weight = t_smooth_weight[0] < 0 ? DEF_SIGMA : t_smooth_weight[0];
+  const double _smooth_var    = t_smooth_var[0] < 0 ? DEF_SIGMA : t_smooth_var[0];
+  const double _repulsion     = t_repulsion[0] < 0 ? DEF_REPULSION : t_repulsion[0];
+  const double _ice_bin_thr   = t_ice_bin_thr[0] < 0 ? DEF_ICE_BIN_THR : t_ice_bin_thr[0];
 
-	plhs[0] = mxCreateDoubleMatrix(1, _col, mxREAL);
-	double *_result = mxGetPr(plhs[0]);
+  double _extra_truth_x[(_num_extra_tr / 2)],
+  _extra_truth_y[(_num_extra_tr / 2)];
+  for (int p = 0; p < (_num_extra_tr / 2); ++p)
+  {
+    _extra_truth_x[p] = t_extra_truth[p * 2];
+    _extra_truth_y[p] = t_extra_truth[(p * 2) + 1];
+  }
 
-	// Call detect class constructor
-	detect obj(_row, _col, _in_data, _slayer, _blayer, _mask,
-			_mu, _sigma, _mid, _egt_weight, _smooth_weight,
-			_smooth_var, _smooth_slope, _bounds, _ms,
-			_num_extra_tr, _extra_truth_x, _extra_truth_y,
-			_result,  _weight_points, _repulsion, _ice_bin_thr);
-}
+  plhs[0] = mxCreateDoubleMatrix(1, _col, mxREAL);
+  double *_result = mxGetPr(plhs[0]);
 
-//  Used to define unary cost of target of position x, y
-double detect::unary_cost(int x, int y)
-{
-	if (debug)
+  // Call detect class constructor
+  detect obj(_row, _col, _in_data, _slayer, _blayer, _mask,
+    _mu, _sigma, _mid, _egt_weight, _smooth_weight,
+    _smooth_var, _smooth_slope, _bounds, _ms,
+    _num_extra_tr, _extra_truth_x, _extra_truth_y,
+    _result,  _weight_points, _repulsion, _ice_bin_thr);
+  }
+
+  //  Used to define unary cost of target of position x, y
+  double detect::unary_cost(int x, int y)
+  {
+    if (debug)
 		mexPrintf("\nRunning unary cost function...");
 
-	// Set cost to large if bottom is above surface
-	if((f_slayer[x] > t) && (f_slayer[x] < f_row - t) && ((y + t + 1) < f_slayer[x]))
+    // Set cost to large if bottom is above surface
+    if((f_slayer[x] > t) && (f_slayer[x] < f_row - t) && ((y + t + 1) < f_slayer[x]))
 		return LARGE;
 
-	// Set cost of center point to large if far from center ground truth
-	if((f_blayer != -1) && (x == f_mid) && (y + t < f_blayer || y + t > f_blayer + 500))
+    // Set cost of center point to large if far from center ground truth
+    if((f_blayer != -1) && (x == f_mid) && (y + t < f_blayer || y + t > f_blayer + 500))
 		return LARGE;
 
-	double cost = 0;
+    double cost = 0;
 
-	// Increase cost if point is far from extra ground truth
-	for(int f = 0; f < (f_num_extra_tr / 2); ++f)
+    // Increase cost if point is far from extra ground truth
+    for(int f = 0; f < (f_num_extra_tr / 2); ++f)
 		if(f_extra_truth_x[f] == x && x < f_bounds[1] && x > f_bounds[0])
 		{
-			cost += f_weight_points[x] * sqr(abs((int)f_extra_truth_y[f] - (int)(t + y)) / f_egt_weight);
-			break;
+		  cost += f_weight_points[x] * sqr(abs((int)f_extra_truth_y[f] - (int)(t + y)) / f_egt_weight);
+		  break;
 		}
 
     // Reduce cost for bins near a region with negative ice mask
-	for(int dist = 0, range = x - f_ice_bin_thr; range <= x + f_ice_bin_thr; ++range)
-	{
-		if(range == x || abs(y - f_slayer[x]) > 40 || range < f_bounds[0] || range > f_bounds[1])
-			continue;
-		if(f_mask[range] == 0)
-		{
-			dist = abs(x - range);
-			cost -= 1000 * dist;
-		}
-	}
+    for(int dist = 0, range = x - f_ice_bin_thr; range <= x + f_ice_bin_thr; ++range)
+    {
+      if(range == x || abs(y - f_slayer[x]) > 40 || range < f_bounds[0] || range > f_bounds[1])
+		continue;
+      if(f_mask[range] == 0)
+      {
+        dist = abs(x - range);
+        cost -= 1000 * dist;
+      }
+    }
 
-	// Increased cost if bottom is close to surface
-	if(abs(y + t - f_slayer[x]) < 10)
-        cost += f_repulsion * (100 - 10 * abs((int)(y + t) - (int)f_slayer[x]));
+    // Increased cost if bottom is close to surface
+    if(abs(y + t - f_slayer[x]) < 10)
+		cost += f_repulsion * (100 - 10 * abs((int)(y + t) - (int)f_slayer[x]));
 
     // Image magnitude correlation
     double tmp_cost = 0;
     for (size_t i = 0; i < f_ms; i++)
-        tmp_cost  -= 2.5 * (f_in_data[encode(x, y + i)]) * f_mu[i] / f_sigma[i];
+		tmp_cost  -= 2.5 * (f_in_data[encode(x, y + i)]) * f_mu[i] / f_sigma[i];
     tmp_cost = tmp_cost*12/10 - 60;
 
     cost += tmp_cost;
 
-	if(debug)
+    if(debug)
 		mexPrintf("\nUnary cost of target (x = %d, y = %d) -> %f", x, y, cost);
 
     return cost;
-}
+  }
 
-// Returns Viterbi solution of optimal path
-double* detect::find_path(void)
-{
+  // Returns Viterbi solution of optimal path
+  double* detect::find_path(void)
+  {
     if (debug)
-		mexPrintf("\nRunning path finding function...");
+    mexPrintf("\nRunning path finding function...");
 
-	start_col   = f_bounds[0];
-	end_col     = f_bounds[1];
+    start_col   = f_bounds[0];
+    end_col     = f_bounds[1];
     t           = (f_ms - 1) / 2;
     depth       = f_row - f_ms;
     num_col_vis = end_col - start_col;
@@ -169,93 +175,93 @@ double* detect::find_path(void)
     double path_prob[depth], path_prob_next[depth], index[depth];
 
     for(int k = 0; k < f_col; ++k)
-        f_result[k] = 0;
+		f_result[k] = 0;
 
     for(int k = 0; k < depth * (num_col_vis + 2); ++k)
-        path[k] = 0;
+		path[k] = 0;
 
     reset_arrays(path_prob, path_prob_next, index);
     viterbi_right(path, path_prob, path_prob_next, index, 0);
-    
+
     int encode;
     int viterbi_index     = calculate_best(path_prob);
     int idx               = end_col;
     f_result[end_col - 1] = f_mask[end_col - 1] == 1 ? viterbi_index + t : f_slayer[end_col - 1];
 
-    // Set result vector 
-    for(int k = start_col + 1; k <= end_col; ++k) 
+    // Set result vector
+    for(int k = start_col + 1; k <= end_col; ++k)
     {
-        encode = vic_encode(viterbi_index, num_col_vis + start_col - k); 
-        viterbi_index = path[encode];
-        f_result[idx - 2] = f_mask[idx - 2] == 1 ? viterbi_index + t : f_slayer[idx - 2];  
-        --idx;
-        if(encode < 0 || idx < 2) 
-            break;       
+      encode = vic_encode(viterbi_index, num_col_vis + start_col - k);
+      viterbi_index = path[encode];
+      f_result[idx - 2] = f_mask[idx - 2] == 1 ? viterbi_index + t : f_slayer[idx - 2];
+      --idx;
+      if(encode < 0 || idx < 2)
+		break;
     }
     delete[] path;
     return f_result;
-}
+  }
 
-// Select path with lowest overall cost
-int detect::calculate_best(double *path_prob)
-{
+  // Select path with lowest overall cost
+  int detect::calculate_best(double *path_prob)
+  {
     double min = LARGE;
     int viterbi_index = 0;
     for(int k = 0; k < depth; ++k)
-        if(path_prob[k] < min)
-        {
-            min = path_prob[k];
-            viterbi_index = k;
-        }
-   return viterbi_index;
-}
+		if(path_prob[k] < min)
+		{
+		  min = path_prob[k];
+		  viterbi_index = k;
+		}
+    return viterbi_index;
+  }
 
-// Reset values of given arrays to 0
-void detect::reset_arrays(double *path_prob, double *path_prob_next, double *index)
-{
+  // Reset values of given arrays to 0
+  void detect::reset_arrays(double *path_prob, double *path_prob_next, double *index)
+  {
     for(int k = 0; k < depth; ++k)
     {
-        path_prob[k] = 0;
-        path_prob_next[k] = 0;
-        index[k] = 0;
+      path_prob[k] = 0;
+      path_prob_next[k] = 0;
+      index[k] = 0;
     }
-}
+  }
 
-// Perform detection to the right
-void detect::viterbi_right(int *path, double *path_prob, double *path_prob_next, double *index, int path_index)
-{
+  // Perform detection to the right
+  void detect::viterbi_right(int *path, double *path_prob, double *path_prob_next, double *index, int path_index)
+  {
     bool next = 0;
     double norm = 0;
     for(int col = start_col; col <= end_col + 1; ++col)
-    {           
-        if(path_index >= depth * (num_col_vis + 2) ||col >= f_col || col < 0)
-          continue;
+    {
+      if(path_index >= depth * (num_col_vis + 2) ||col >= f_col || col < 0)
+		continue;
 
-        norm = norm_pdf(col, (double)f_mid, f_smooth_var, f_smooth_weight);
+      norm = norm_pdf(col, (double)f_mid, f_smooth_var, f_smooth_weight);
 
-        if(!next)
-            dt_1d(path_prob, norm, path_prob_next, index, 0, depth, f_smooth_slope[col-1]);
-        else
-            dt_1d(path_prob_next, norm, path_prob, index, 0, depth, f_smooth_slope[col-1]);
+      if(!next)
+		dt_1d(path_prob, norm, path_prob_next, index, 0, depth, f_smooth_slope[col-1]);
+      else
+		dt_1d(path_prob_next, norm, path_prob, index, 0, depth, f_smooth_slope[col-1]);
 
-        if(f_mask[col] == 0 && f_slayer[col] > t)
-            for(int row = 0; row < depth; ++row)
-            {
-                path[path_index] = index[row];
-                if(row + t != f_slayer[col])
-                {
-                    if(!next) path_prob_next[row] += LARGE;
-                    else      path_prob[row] += LARGE;
-                }
-                ++path_index;
-            }
-        else for(int row = 0; row < depth; ++row)
-            {
-                path[path_index] = index[row];
-                if(!next) path_prob_next[row] += unary_cost(col, row);
-                else      path_prob[row] += unary_cost(col, row);
-                ++path_index;
-            }
-        next = !next;
-   }
-}
+      if(f_mask[col] == 0 && f_slayer[col] > t)
+		  for(int row = 0; row < depth; ++row)
+		  {
+			path[path_index] = index[row];
+			if(row + t != f_slayer[col])
+			{
+			  if(!next) path_prob_next[row] += LARGE;
+			  else      path_prob[row] += LARGE;
+			}
+			++path_index;
+		  }
+      else for(int row = 0; row < depth; ++row)
+      {
+        path[path_index] = index[row];
+        if(!next) path_prob_next[row] += unary_cost(col, row);
+        else      path_prob[row] += unary_cost(col, row);
+        ++path_index;
+      }
+      next = !next;
+    }
+  }
--- cresis-toolbox/+tomo/viterbi_lib.h
+++ cresis-toolbox/+tomo/viterbi_lib.h
@@ -1,3 +1,12 @@
+// viterbi_lib.h
+//
+// Layer-tracking program based on the Viterbi algorithm
+//
+// Adapted from original code by Mingze Xu, David Crandall, and John Paden
+//
+// Author: Victor Berger
+// See also: viterbi.cpp
+
 #ifndef _VITERBI_H_
 #define _VITERBI_H_
 
@@ -14,100 +23,100 @@ const double LARGE           = 1000000000;
 
 class detect
 {
-	public:
-		detect( const int d_row,               const int d_col,
-				const double *d_in_data,       const int *d_slayer,
-				const int d_blayer,            const double *d_mask,
-				const double *d_mu,            const double *d_sigma,
-				const int d_mid,               const double d_egt_weight,
-				const double d_smooth_weight,  const double d_smooth_var,
-				const double *d_smooth_slope,  const ptrdiff_t *d_bounds,
-				const size_t d_ms,             const int d_num_extra_tr,
-				const double *d_extra_truth_x, const double *d_extra_truth_y,
-				double *d_result,			   const double *d_weight_points,
-                const double d_repulsion,      const double d_ice_bin_thr
-              )
-            : f_row(d_row),                     f_col(d_col),
-			  f_in_data(d_in_data),             f_slayer(d_slayer),
-			  f_blayer(d_blayer),               f_mask(d_mask),
-			  f_mu(d_mu),                       f_sigma(d_sigma),
-			  f_mid(d_mid),                     f_egt_weight(d_egt_weight),
-			  f_smooth_weight(d_smooth_weight), f_smooth_var(d_smooth_var),
-			  f_smooth_slope(d_smooth_slope),   f_bounds(d_bounds),
-			  f_ms(d_ms),                       f_num_extra_tr(d_num_extra_tr),
-			  f_extra_truth_x(d_extra_truth_x), f_extra_truth_y(d_extra_truth_y),
-			  f_result(d_result),               f_weight_points(d_weight_points),		  
-			  f_repulsion(d_repulsion),         f_ice_bin_thr(d_ice_bin_thr)
-        {
-            find_path();
-            mexPrintf("\n");
-        }
-		// VARIABLES
-		const int f_row, f_col, f_mid, f_blayer, 
-                  *f_slayer, f_num_extra_tr;
-		const double *f_in_data, *f_mask, *f_mu, *f_sigma, f_egt_weight,
-                     *f_smooth_slope, *f_extra_truth_x, *f_extra_truth_y,
-                     *f_weight_points,f_smooth_weight, f_smooth_var, f_repulsion, f_ice_bin_thr;
-		const ptrdiff_t *f_bounds;
-		const size_t f_ms;
-		double *f_result;
-        int depth, num_col_vis, t, start_col, end_col;
+public:
+	detect( const int d_row,               const int d_col,
+		const double *d_in_data,       const int *d_slayer,
+		const int d_blayer,            const double *d_mask,
+		const double *d_mu,            const double *d_sigma,
+		const int d_mid,               const double d_egt_weight,
+		const double d_smooth_weight,  const double d_smooth_var,
+		const double *d_smooth_slope,  const ptrdiff_t *d_bounds,
+		const size_t d_ms,             const int d_num_extra_tr,
+		const double *d_extra_truth_x, const double *d_extra_truth_y,
+		double *d_result,			   const double *d_weight_points,
+		const double d_repulsion,      const double d_ice_bin_thr
+	)
+	: f_row(d_row),                     f_col(d_col),
+	f_in_data(d_in_data),             f_slayer(d_slayer),
+	f_blayer(d_blayer),               f_mask(d_mask),
+	f_mu(d_mu),                       f_sigma(d_sigma),
+	f_mid(d_mid),                     f_egt_weight(d_egt_weight),
+	f_smooth_weight(d_smooth_weight), f_smooth_var(d_smooth_var),
+	f_smooth_slope(d_smooth_slope),   f_bounds(d_bounds),
+	f_ms(d_ms),                       f_num_extra_tr(d_num_extra_tr),
+	f_extra_truth_x(d_extra_truth_x), f_extra_truth_y(d_extra_truth_y),
+	f_result(d_result),               f_weight_points(d_weight_points),
+	f_repulsion(d_repulsion),         f_ice_bin_thr(d_ice_bin_thr)
+	{
+		find_path();
+		mexPrintf("\n");
+	}
+	// VARIABLES
+	const int f_row, f_col, f_mid, f_blayer,
+		*f_slayer, f_num_extra_tr;
+	const double *f_in_data, *f_mask, *f_mu, *f_sigma, f_egt_weight,
+		*f_smooth_slope, *f_extra_truth_x, *f_extra_truth_y,
+		*f_weight_points,f_smooth_weight, f_smooth_var, f_repulsion, f_ice_bin_thr;
+	const ptrdiff_t *f_bounds;
+	const size_t f_ms;
+	double *f_result;
+	int depth, num_col_vis, t, start_col, end_col;
 
-		// METHODS
-        int calculate_best(double *path_prob);
-		double unary_cost(int x, int y), *find_path(void);
-        void viterbi_right(int *path, double *path_prob, double *path_prob_next, double *index, int path_index), 
-             reset_arrays(double *path_prob, double *path_prob_next, double *index);
+	// METHODS
+	int calculate_best(double *path_prob);
+	double unary_cost(int x, int y), *find_path(void);
+	void viterbi_right(int *path, double *path_prob, double *path_prob_next, double *index, int path_index),
+	reset_arrays(double *path_prob, double *path_prob_next, double *index);
 
-		template <class T>
-			T sqr(T x) { return x*x; }
+	template <class T>
+	T sqr(T x) { return x*x; }
 
-		double norm_pdf(int x, double mu = DEF_MID,
-				double sigma = DEF_SIGMA, double scale = DEF_SCALE)
+	double norm_pdf(int x, double mu = DEF_MID,
+		double sigma = DEF_SIGMA, double scale = DEF_SCALE)
 		{
-            if(sigma != std::numeric_limits<double>::infinity())
-                return scale * (1.0/(sigma*sqrt(2*M_PI))) * exp(-0.5*sqr((x-mu)/sigma));
-            return scale;
+			if(sigma != std::numeric_limits<double>::infinity())
+				return scale * (1.0/(sigma*sqrt(2*M_PI))) * exp(-0.5*sqr((x-mu)/sigma));
+			return scale;
 		}
 
 		int encode(int x, int y)
-		{ 
+		{
 			return x * f_row + y;
 		}
-		
+
 		int vic_encode(int row, int col)
-        {
-            return depth * col + row;
-        }
+		{
+			return depth * col + row;
+		}
 
 		//  Distance transform calculation, adapted from David Crandall
 		//  Every index from d1 to d2 will be set in dst and dst_ind
 		//  dst will contain the minimum value for that destination
 		//  dst_ind will contain the minimum source index for that destination
 		void dt(const double *src, double *dst, double *dst_ind, int s1, int s2,
-						int d1, int d2, double scale, int off = 0)
-		{
+			int d1, int d2, double scale, int off = 0)
+			{
 
-			int d = (d1 + d2) >> 1, s = s1; // Find the midpoint of the destination
-			for (int p = s1; p <= s2; p++) // Search through all the sources and find the minimum
-				if (src[s] + sqr(s-d-off) * scale > src[p] + sqr(p-d-off) * scale)
-            		s = p;
-                
-			dst[d] = src[s] + sqr(s-d-off) * scale; // Minimum value to the midpoint
+				int d = (d1 + d2) >> 1, s = s1; // Find the midpoint of the destination
+				for (int p = s1; p <= s2; p++) // Search through all the sources and find the minimum
+					if (src[s] + sqr(s-d-off) * scale > src[p] + sqr(p-d-off) * scale)
+						s = p;
 
-			dst_ind[d] = s; // Minimum source index for the midpoint
+				dst[d] = src[s] + sqr(s-d-off) * scale; // Minimum value to the midpoint
 
-			if(d2 >= d + 1) // Recursive call, binary search (top half of destinations)
-				dt(src, dst, dst_ind, s, s2, d+1, d2, scale, off);
+				dst_ind[d] = s; // Minimum source index for the midpoint
 
-			if(d-1 >= d1) // Recursive call, binary search (bottom half of destinations)
-				dt(src, dst, dst_ind, s1, s, d1, d-1, scale, off);
-		}
+				if(d2 >= d + 1) // Recursive call, binary search (top half of destinations)
+					dt(src, dst, dst_ind, s, s2, d+1, d2, scale, off);
 
-		void dt_1d(const double *f, double scale, double *result,
-			         double *dst_ind, int beg, int end, int off = 0)
-		{
-			dt(f, result, dst_ind, beg, end-1, beg, end-1, scale, off);
-		}
-}; 
-#endif
+				if(d-1 >= d1) // Recursive call, binary search (bottom half of destinations)
+					dt(src, dst, dst_ind, s1, s, d1, d-1, scale, off);
+			}
+
+			void dt_1d(const double *f, double scale, double *result,
+				double *dst_ind, int beg, int end, int off = 0)
+				{
+					dt(f, result, dst_ind, beg, end-1, beg, end-1, scale, off);
+				}
+			};
+			#endif
