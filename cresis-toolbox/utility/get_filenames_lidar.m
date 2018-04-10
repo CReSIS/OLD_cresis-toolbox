0fe60883dd546e1bc37d20076067c69cba0e602d
John Paden <paden@ku.edu>
2018-04-10 14:49:55 -0500

    get_filenames_lidar.m: Fixed to work with AWI data files

--- cresis-toolbox/utility/get_filenames_lidar.m
+++ cresis-toolbox/utility/get_filenames_lidar.m
@@ -8,14 +8,16 @@ function lidar_fns = get_filenames_lidar(param,lidar_source,gps_time)
 %   param: structure controlling which LIDAR files are loaded
 %     .season_name: string containing the season that the data is from
 %   gps_time: start and stop times in ANSI-C time (seconds since Jan 1,
-%     1970). Used to determine which files to get.
+%     1970). Used to determine which files to get. Note that all files in a
+%     day will be returned even if a single gps_time is given. The
+%     start/stop is only necessary for crossing date boundaries.
 %   lidar_source: string containing 'awi' or 'dtu'
 %
 % Output:
 %   lidar_fns: (cell of strings) lidar filenames (same as get_filenames output)
 %
 % Examples:
-%   lidar_fns = get_filenames_lidar(struct('season_name','2015_Greenland_Polar6'),'20150913')
+%   lidar_fns = get_filenames_lidar(struct('season_name','2015_Greenland_Polar6'),'awi',datenum_to_epoch(datenum('20150913','YYYYmmDD')))
 %
 % Author: John Paden
 %
@@ -31,15 +33,14 @@ end
 in_base_path = fullfile(data_support_path,sprintf('LIDAR_%s',upper(lidar_source)),param.season_name);
 
 if strcmpi(lidar_source,'awi')
-  lidar_fns = get_filenames(in_base_path, 'ALS_L1B', filename_middle, '.nc');
-  start_time = epoch_to_datenum(gps_time(1));
-  end_time = epoch_to_datenum(gps_time(end));
+  [year month day] = datevec(epoch_to_datenum(gps_time(1)));
+  day_of_year_start = datenum(year,month,day) - datenum(year,0,0);
+  [year month day] = datevec(epoch_to_datenum(gps_time(end)));
+  day_of_year_end = datenum(year,month,day) - datenum(year,0,0);
   lidar_fns = {};
-  cur_time = start_time;
-  while cur_time <= end_time
-    new_fns = get_filenames(in_base_path, 'ALS_L1B_%s', sprintf('%.0f_',day_of_year), '', '_1x1.ver');
+  for day_of_year = day_of_year_start:day_of_year_end
+    new_fns = get_filenames(in_base_path, sprintf('ALS_L1B_%sT',datestr(datenum(year,0,day_of_year),'YYYYmmDD')), '', '.nc');
     lidar_fns(end+(1:length(new_fns)),1) = new_fns;
-    cur_time = cur_time + 1;
   end
 
 else
