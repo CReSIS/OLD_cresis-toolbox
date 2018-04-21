45b6654092e7ccd9895106d126e76eb7d6dc5f0b
John Paden <paden@ku.edu>
2018-04-21 02:07:41 -0200

    data loader updates, incomplete

--- cresis-toolbox/processing/data_load.m
+++ cresis-toolbox/processing/data_load.m
@@ -1,37 +1,20 @@
-function [hdr,data] = data_load(param,records)
+function [hdr,data] = data_load(param,wfs,records)
 % [hdr,data] = data_load(param,records)
 
 %% Preallocate data
 % ===================================================================
-[accum,state] = build_img_load_struct(param.load.imgs, param.load.adcs, ...
-  struct('file_version',param.load.file_version));
-
-total_rec = length(param.load.file_idx{1});
+total_rec = param.load.recs(end)-param.load.recs(1)+1;
 Nx = floor(total_rec/param.proc.presums);
-if ~iscell(data)
-  data = cell(size(param.load.imgs));
-end
-for img_idx = 1:length(param.load.imgs)
-  wf = abs(param.load.imgs{img_idx}(1,1));
-  if param.load.wf_adc_comb.en
-    Nt = param.load.wf_adc_comb.Nt;
+data = cell(size(param.load.imgs));
+for img = 1:length(param.load.imgs)
+  wf = abs(param.load.imgs{img}(1,1));
+  if param.load.pulse_comp
+    Nt = param.wfs(wf).Nt;
   else
-    if param.proc.raw_data
-      Nt = param.wfs(wf).Nt_raw;
-    else
-      Nt = param.wfs(wf).Nt;
-    end
+    Nt = param.wfs(wf).Nt_raw;
   end
-  if param.proc.combine_rx
-    Nc = 1;
-  else
-    Nc = size(param.load.imgs{img_idx},1);
-  end
-  if any([size(data{img_idx},1) size(data{img_idx},2) size(data{img_idx},3)] ~= [Nt Nx Nc])
-    data{img_idx} = zeros(Nt,Nx,Nc,'single');
-  elseif param.proc.combine_rx
-    data{img_idx}(:) = 0;
-  end
+  Nc = size(param.load.imgs{img},1);
+  data{img} = zeros(Nt,Nx,Nc,'single');
 end
 
 %% Load data
@@ -39,77 +22,63 @@ end
 for board_idx = 1:length(boards)
   board = boards(board_idx);
   
-  while rel_rec = 1:total_rec;
+  while rec = 1:total_rec;
     
     % Load record
     [state] = load_radar_data_fh(fns,offset,state);
     
-    % Process all adc-wf pairs in this record
-    for accum_idx = 1:length(accum(board+1).wf)
-      adc = accum(board+1).adc(accum_idx);
-      wf = accum(board+1).wf(accum_idx);
-      
-      else
+    if ~param.proc.raw_data
+      % Process all adc-wf pairs in this record
+      for accum_idx = 1:length(state(board+1).wf)
+        adc = state(board+1).adc(accum_idx);
+        wf = state(board+1).wf(accum_idx);
+        
         % Convert from quantization to voltage at the receiver input for the
         % maximum gain case:
         %  1. fast time gains less than the maximum for this record will be
-        %     undone in the next step
+        %     compensated for in the next step
         %  2. antenna effects not considered at this step
         state.data{adc,wf} = state.data{adc,wf} * wfs(wf).quantization_to_V * state.hdr{adc,wf}.quantization_to_V_adjustment;
         
         % Apply fast-time varying gain
         state.data{adc,wf} = state.data{adc,wf} .* interp1(wfs(wf).gain.Time, wfs(wf).gain.Gain, wfs(wf).time_raw(1:wfs(wf).Nt_raw));
-        old_tmp_data{adc,wf} = tmp_data{adc,wf};
+      end
     end
     
     % Accumulate (presum)
     if adc<=size(tmp_data,1) && wf<=size(tmp_data,2) && ~isempty(tmp_data{adc,wf})
       if num_accum == 0
-        accum(board+1).data{accum_idx} = tmp_data{adc,wf};
+        state(board+1).data{accum_idx} = state.data{adc,wf};
       else
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx} + tmp_data{adc,wf};
+        state(board+1).data{accum_idx} = state(board+1).data{accum_idx} + tmp_data{adc,wf};
       end
     else
       if num_accum == 0
-        accum(board+1).data{accum_idx} = tmp_data{adc,wf};
+        state(board+1).data{accum_idx} = tmp_data{adc,wf};
       else
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx} + tmp_data{adc,wf};
+        state(board+1).data{accum_idx} = state(board+1).data{accum_idx} + tmp_data{adc,wf};
       end
     end
   end
   
   num_accum = num_accum + 1;
-  if num_accum >= param.proc.presums
+  if num_accum >= param.load.presums
     num_accum = 0;
     out_idx = out_idx + 1;
-    for accum_idx = 1:length(accum(board+1).wf)
-      adc = accum(board+1).adc(accum_idx);
-      wf = accum(board+1).wf(accum_idx);
-      img_idx = accum(board+1).img_idx(accum_idx);
-      wf_adc_idx = accum(board+1).wf_adc_idx(accum_idx);
-      iq_mode = accum(board+1).iq_mode(accum_idx);
-      zero_pi_mode = accum(board+1).zero_pi_mode(accum_idx);
-      
-      % Combine I&Q channels if necessary
-      if iq_mode == 1
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx-1};
+    for accum_idx = 1:length(state(board+1).wf)
+      adc = state(board+1).adc(accum_idx);
+      wf = state(board+1).wf(accum_idx);
+      img = state(board+1).img(accum_idx);
+      wf_adc_idx = state(board+1).wf_adc_idx(accum_idx);
+      wf_adc_sum = state(board+1).wf_adc_sum(accum_idx);
+      wf_adc_sum_done = state(board+1).wf_adc_sum_done(accum_idx);
+
+      % Sum up wf-adc pairs until done
+      state(board+1).data{accum_idx} = state(board+1).data{accum_idx} + 1i*sign(iq_mode)*state(board+1).data{accum_idx};
+      if ~wf_adc_sum_done
         continue;
-      elseif abs(iq_mode) >= 2
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx} + 1i*sign(iq_mode)*accum(board+1).data{accum_idx};
       end
-      
-      % Combine zero and pi channels if necessary
-      if zero_pi_mode == 1
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx-1};
-        continue;
-      elseif zero_pi_mode >= 2
-        accum(board+1).data{accum_idx} = accum(board+1).data{accum_idx} + sign(zero_pi_mode)*accum(board+1).data{accum_idx};
-      end
-      
-      % Store wf-adc pair in output matrix
-      data{img_idx}(:,out_idx,wf_adc_idx) = accum(board+1).data{accum_idx} / param.proc.presums;
-    end
-    
+ 
   end
 end
 
