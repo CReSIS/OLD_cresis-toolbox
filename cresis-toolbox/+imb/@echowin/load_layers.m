d223e26979a4a99a948466b330470b8fb83df830
Anjali Pare <a981p886@ku.edu>
2019-01-10 13:20:17 -0600

    load_layers.m: Partially complete loading from layerData

--- cresis-toolbox/+imb/@echowin/load_layers.m
+++ cresis-toolbox/+imb/@echowin/load_layers.m
@@ -1,8 +1,16 @@
 function load_layers(obj)
+frm=2;
 % echowin.load_layers(obj)
 %
 % Load layer information from database and update layer plot handles
 
+%Loading frames file  
+  %load(ct_filename_support(param,'','frames'));
+  %for frm_idx=1:length(param.frms)
+  %frm=param.frms(frm_idx);
+  
+% if strcmpi(obj.source,'OPS')
+% OPS: Determine GPS time domain to load from data base
 dx = obj.eg.gps_time(2)-obj.eg.gps_time(1);
 min_gps_time = obj.eg.gps_time(1);
 max_gps_time = obj.eg.gps_time(end);
@@ -17,7 +25,9 @@ if obj.eg.frame_idxs(end) == length(obj.eg.stop_gps_time)
   max_gps_time = obj.eg.stop_gps_time(end)+dx;
 end
 
+% OPS: Load layer points from database
 fprintf(' Loading layer points from database (%s)\n',datestr(now,'HH:MM:SS'));
+if 0
 ops_param = struct('properties',[]);
 ops_param.properties.location = obj.eg.cur_sel.location;
 ops_param.properties.season = obj.eg.cur_sel.season_name;
@@ -28,19 +38,22 @@ ops_param.properties.lyr_name = obj.eg.layer.layer_names;
 % query database
 [status,data] = opsGetLayerPoints(obj.eg.system,ops_param);
 
-layer_type = data.properties.lyr_id;
-% Preallocate layer arrays 
+% OPS: Preallocate layer arrays 
 obj.eg.layer.x = {};
 obj.eg.layer.y = {};
 obj.eg.layer.qual = {};
 obj.eg.layer.type = {};
 for idx = 1:length(obj.eg.layer_id)
   obj.eg.layer.x{idx} = double(obj.eg.map_gps_time); % gps-time
-  obj.eg.layer.y{idx} = NaN*zeros(size(obj.eg.map_id)); % twtt
-  obj.eg.layer.qual{idx} = NaN*zeros(size(obj.eg.map_id)); % integer 1-3
-  obj.eg.layer.type{idx} = NaN*zeros(size(obj.eg.map_id)); % this is either 1 (manual) or 2 (auto)
+  obj.eg.layer.y{idx} = nan(size(obj.eg.map_id)); % twtt
+  obj.eg.layer.qual{idx} = nan(size(obj.eg.map_id)); % integer 1-3
+  obj.eg.layer.type{idx} = nan(size(obj.eg.map_id)); % this is either 1 (manual) or 2 (auto)
 end
-% Fill layer arrays with points from database
+
+% OPS: Fill layer arrays with points from database
+% start with layer Data gps time, layer Data
+% twtt, ops gps time and that would give you the twtt interpolated onto the map or OPS
+% time. Check for quality and type. 
 for idx = 1:length(data.properties.point_path_id)
   layer_idx = obj.eg.layers.lyr_id == data.properties.lyr_id(idx);
   point_path_idx = obj.eg.map_id == data.properties.point_path_id(idx);
@@ -49,6 +62,38 @@ for idx = 1:length(data.properties.point_path_id)
   obj.eg.layer.type{layer_idx}(point_path_idx) = data.properties.type(idx);
 end
 
+elseif 1
+  layer_fn=fullfile(ct_filename_out(obj.eg.cur_sel,'layerData',''),sprintf('Data_%s_%03d.mat','20110502_01',frm));
+  %layer_fn=fullfile('X:\ct_data\rds\2011_Greenland_P3\CSARP_standard\20110502_01',frm);
+  lay=load(layer_fn);
+  %Preallocate layer arrays
+  obj.eg.layer.x = {};
+  obj.eg.layer.y = {};
+  obj.eg.layer.qual = {};
+  obj.eg.layer.type = {};
+  for idx = 1:length(obj.eg.layer_id)
+    %obj.eg.layer.x{idx} = double(obj.eg.map_gps_time); % gps-time
+    obj.eg.layer.x{idx}=lay.GPS_time;
+    obj.eg.layer.y{idx} = nan(size(obj.eg.map_id)); % twtt
+    obj.eg.layer.qual{idx} = nan(size(obj.eg.map_id)); % integer 1-3
+    obj.eg.layer.type{idx} = nan(size(obj.eg.map_id)); % this is either 1 (manual) or 2 (auto)
+  end
+  %Filling the layers
+  for idx=1:length(obj.eg.layer_id)
+    %for lay_idx=1:lay.layerData;
+    % y has twtt of the layer data interpolated on the echogram GPS time
+      %y=interp1(lay.GPS_time, lay.layerData{idx}.value{2}.data, obj.eg.layer.x{idx});
+      %obj.eg.layer.y{idx}=y;
+      obj.eg.layer.y{idx}=lay.layerData{idx}.value{2}.data;
+      obj.eg.layer.qual{idx}=lay.layerData{idx}.quality;
+      %if not finite fill the type with 2 else 1
+      if ~isfinite(lay.layerData{idx}.value{2}.data)
+        obj.eg.layer.type{idx}= 2*ones(size(lay.GPS_time));
+      else
+        obj.eg.layer.type{idx}=ones(size(lay.GPS_time)); 
+      end
+  end
+end   
 %% Update echogram surface if there are enough good points from OPS
 % Find good surface points
 if ~any(obj.eg.layer_id==1)
