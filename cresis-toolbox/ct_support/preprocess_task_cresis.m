3a4b906dcd2a7efdd22467425b9ea24ee7c117f7
John Paden <paden@ku.edu>
2019-03-13 13:02:55 +0100

    preprocess_task_cresis: handle directory creation for board_hdrs file run_preprocess_AWI: updates for 2019 field season default_radar_params_2017_Arctic_Polar5_snow: corrected some parameters default_radar_params_2019_Arctic_Polar5_snow: corrected some parameters and made single channel

--- cresis-toolbox/ct_support/preprocess_task_cresis.m
+++ cresis-toolbox/ct_support/preprocess_task_cresis.m
@@ -702,6 +702,10 @@ for board_idx = 1:num_board_to_load
 end
 
 % Save concatenated temporary file
+fn_board_hdrs_dir = fileparts(fn_board_hdrs);
+if ~exist(fn_board_hdrs_dir,'dir')
+  mkdir(fn_board_hdrs_dir);
+end
 save(fn_board_hdrs,'-v7.3','board_hdrs','fns_list','failed_load');
 
 %% List bad files
--- cresis-toolbox/ct_support/run_preprocess_AWI.m
+++ cresis-toolbox/ct_support/run_preprocess_AWI.m
@@ -14,12 +14,12 @@ param.config.default = [];
 % param.config.date_strs{cur_idx} = '20180405';
 
 %% SNOW8 SINGLE DAY
-% cur_idx = length(param.config.default)+1;
-% param.config.default{cur_idx} = default_radar_params_2017_Arctic_Polar5_snow();
-% param.config.base_dir{cur_idx} = '/work/ollie/ajutila/Data/mnt/HDD5/';
-% param.config.config_folder_names{cur_idx} = '1703301101/UWBM/';
-% param.config.board_folder_names{cur_idx} = '1703301101/UWBM/%b';
-% param.config.date_strs{cur_idx} = '20170330';
+cur_idx = length(param.config.default)+1;
+param.config.default{cur_idx} = default_radar_params_2019_Arctic_Polar5_snow();
+param.config.base_dir{cur_idx} = '/work/ollie/ajutila/Data/mnt/HDD5/';
+param.config.config_folder_names{cur_idx} = '1903080101/UWBM/';
+param.config.board_folder_names{cur_idx} = '1903080101/UWBM/%b';
+param.config.date_strs{cur_idx} = '20190308';
 
 %% MCORDS5-ACCUM SINGLE DAY
 % cur_idx = length(param.config.default)+1;
@@ -47,18 +47,18 @@ param.config.default = [];
   % param.config.date_strs{cur_idx} = date_strs{idx};
 % end
 
-%% SNOW8 MULTIPLE DAYS
-date_strs = {'1703301101','1704021401','1704041501','1704061601','1704082001','1704102101'};
-config_format_str = '%s/UWBM/';
-board_format_str = '%s/UWBM/%%b';
-defaults_fh = @default_radar_params_2017_Arctic_Polar5_snow;
-base_dir = '/work/ollie/ajutila/Data/mnt/HDD5/';
-
-for idx = 1:length(date_strs)
-  cur_idx = length(param.config.default)+1;
-  param.config.default{cur_idx} = defaults_fh();
-  param.config.base_dir{cur_idx} = base_dir;
-  param.config.config_folder_names{cur_idx} = sprintf(config_format_str,date_strs{idx});
-  param.config.board_folder_names{cur_idx} = sprintf(board_format_str,date_strs{idx});
-  param.config.date_strs{cur_idx} = ['20' date_strs{idx}(1:6)];
-end
+% %% SNOW8 MULTIPLE DAYS
+% date_strs = {'1703301101','1704021401','1704041501','1704061601','1704082001','1704102101'};
+% config_format_str = '%s/UWBM/';
+% board_format_str = '%s/UWBM/%%b';
+% defaults_fh = @default_radar_params_2017_Arctic_Polar5_snow;
+% base_dir = '/work/ollie/ajutila/Data/mnt/HDD5/';
+% 
+% for idx = 1:length(date_strs)
+%   cur_idx = length(param.config.default)+1;
+%   param.config.default{cur_idx} = defaults_fh();
+%   param.config.base_dir{cur_idx} = base_dir;
+%   param.config.config_folder_names{cur_idx} = sprintf(config_format_str,date_strs{idx});
+%   param.config.board_folder_names{cur_idx} = sprintf(board_format_str,date_strs{idx});
+%   param.config.date_strs{cur_idx} = ['20' date_strs{idx}(1:6)];
+% end
