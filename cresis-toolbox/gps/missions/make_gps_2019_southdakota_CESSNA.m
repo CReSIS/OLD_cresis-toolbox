73c7ddeecfeb8c977e8215b81278de82192bfc4e
YOUR_NAME <WORK_EMAIL_ADDRESS>
2020-01-23 12:52:50 -0600

    updates for outh dakota missions

--- cresis-toolbox/gps/missions/make_gps_2019_southdakota_CESSNA.m
+++ cresis-toolbox/gps/missions/make_gps_2019_southdakota_CESSNA.m
@@ -37,7 +37,16 @@ sync_fns = {}; sync_params = {};
 
 gps_source_to_use = 'NMEA';
 if strcmpi(gps_source_to_use,'NMEA')
-    year = 2019; month = 12; day = 11 ;
+%     year = 2019; month = 12; day = 11 ;
+%     file_idx = file_idx + 1;
+%     in_fns{file_idx} = get_filenames(fullfile(in_base_path,sprintf('%04d%02d%02d',year,month,day)),'nmea','','.gps');
+%     out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
+%     file_type{file_idx} = 'NMEA';
+%     params{file_idx} = struct('year',year,'month',month,'day',day,'format',3,'time_reference','utc');
+%     gps_source{file_idx} = 'nmea-field';
+%     sync_flag{file_idx} = 0;
+    
+    year = 2020; month = 01; day = 16;
     file_idx = file_idx + 1;
     in_fns{file_idx} = get_filenames(fullfile(in_base_path,sprintf('%04d%02d%02d',year,month,day)),'nmea','','.gps');
     out_fns{file_idx} = sprintf('gps_%04d%02d%02d.mat', year, month, day);
