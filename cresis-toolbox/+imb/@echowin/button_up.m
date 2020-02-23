174e23f58193003e49b8b2599b76a4800fa932e2
John Paden <paden@ku.edu>
2020-02-23 07:11:40 -0600

    imb.picker: fix left click/right click zoom on edges of the image where nonlinear GPS times exist

--- cresis-toolbox/+imb/@echowin/button_up.m
+++ cresis-toolbox/+imb/@echowin/button_up.m
@@ -118,11 +118,16 @@ if obj.zoom_mode
       return;
     end
     zooms = -1.5;
+    
     cur_axis = axis(obj.h_axes);
     y_extent = cur_axis(4) - cur_axis(3);
     x_extent = cur_axis(2) - cur_axis(1);
-    xlims = [x - x_extent*2^zooms, x + x_extent*2^zooms];
-    ylims = [y - y_extent*2^zooms, y + y_extent*2^zooms];
+    
+    % Zoom so that the mouse pointer's position in the echogram does not change
+    x_percent = (x-cur_axis(1))/x_extent;
+    y_percent = (y-cur_axis(3))/y_extent;
+    xlims = [x - x_extent*2^(zooms+1)*x_percent, x + x_extent*2^(zooms+1)*(1-x_percent)];
+    ylims = [y - y_extent*2^(zooms+1)*y_percent, y + y_extent*2^(zooms+1)*(1-y_percent)];
     
     % Convert x_min, x_max to GPS time
     xlims = interp1(obj.eg.image_xaxis,obj.eg.image_gps_time,xlims,'linear','extrap');
@@ -134,11 +139,16 @@ if obj.zoom_mode
   elseif but == 3
     %% Right click: Zoom out at point
     zooms = -0.5;
+    
     cur_axis = axis(obj.h_axes);
     y_extent = cur_axis(4) - cur_axis(3);
     x_extent = cur_axis(2) - cur_axis(1);
-    xlims = [x - x_extent*2^zooms, x + x_extent*2^zooms];
-    ylims = [y - y_extent*2^zooms, y + y_extent*2^zooms];
+    
+    % Zoom so that the mouse pointer's position in the echogram does not change
+    x_percent = (x-cur_axis(1))/x_extent;
+    y_percent = (y-cur_axis(3))/y_extent;
+    xlims = [x - x_extent*2^(zooms+1)*x_percent, x + x_extent*2^(zooms+1)*(1-x_percent)];
+    ylims = [y - y_extent*2^(zooms+1)*y_percent, y + y_extent*2^(zooms+1)*(1-y_percent)];
     
     % Convert x_min, x_max to GPS time
     xlims = interp1(obj.eg.image_xaxis,obj.eg.image_gps_time,xlims,'linear','extrap');
