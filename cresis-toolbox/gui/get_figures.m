fcb3532dc8c3db059b78d5dfe94be490ceb9611a
John Paden <paden@ku.edu>
2020-04-15 23:06:01 -0500

    get_figures: handle failure while getting children of figure 0

--- cresis-toolbox/gui/get_figures.m
+++ cresis-toolbox/gui/get_figures.m
@@ -32,7 +32,13 @@ if visible_flag
 else
   fig_visible = 'off';
 end
-h_figs = get(0,'Children');
+try
+  h_figs = get(0,'Children');
+  asdf
+catch ME
+  warning(ME.getReport);
+  h_figs = [];
+end
 
 if num_figs <= 0
   h_fig = [];
