5f28fdb5c3283183060a542131e03ecd6fd5a97d
Anjali Pare <anjali.pare@ku.edu>
2020-04-16 18:28:59 -0500

    Update on tuning of temporary files

--- cresis-toolbox/tracker/cluster_tmp_tuning.m
+++ cresis-toolbox/tracker/cluster_tmp_tuning.m
@@ -36,11 +36,11 @@ fprintf('=====================================================================\n
 %     break;
 %   end
 % end
-frm=1;
-temp = load('/cresis/snfs1/dataproducts/ct_data/ct_tmp/layer_tracker/rds/2014_Greenland_P3/20140516_01_20200416_002026_t032_lsm.mat');
+
+temp = load('/cresis/snfs1/dataproducts/ct_data/ct_tmp/layer_tracker/rds/2014_Greenland_P3/20140516_01_20200416_181841_t032_lsm.mat');
 param= temp.param;
-res_matrix_surface = zeros(9,16);
-res_matrix_bottom = zeros(9,16);
+res_matrix_surface = zeros([length(param.cmd.frms),param.layer_tracker.track{1}.idx_reshape]);
+res_matrix_bottom = zeros([length(param.cmd.frms),param.layer_tracker.track{1}.idx_reshape]);
 layer_params = [];
 idx = 0;
 idx = idx + 1;
@@ -53,16 +53,26 @@ layer_params(idx).source = 'layerdata';
 layer_params(idx).layerdata_source = 'layer';
 layers = opsLoadLayers(param,layer_params);
 filename = '/cresis/snfs1/dataproducts/ct_data/rds/2014_Greenland_P3/CSARP_layer_tracker_tmp/CSARP_layer_tune/20140516_01/';
-for track_idx = 1:length(param.layer_tracker.track)
-  for frm = param.cmd.frms
+save_name = '/cresis/snfs1/scratch/anjali/cluster_tuning/';
+num_gt_isfinite_surf = 0;
+num_isnan_surf = 0;
+num_points_surf = 0;
+
+num_gt_isfinite_bot = 0;
+num_isnan_bot = 0;
+num_points_bot = 0;
+
+for track_idx = 1:3
+  frm_idx = 1;
+  for frm = [40 41]
     data_fn_dir = ct_filename_out(param, param.layer_tracker.echogram_source, '');
     data_fn_name = sprintf('Data_%s_%03d.mat',param.day_seg,frm);
     data_fn      = fullfile(data_fn_dir, data_fn_name);
     data = load(data_fn);
     frm_dir = sprintf('layer_tracker_%03d',frm);
-    surf = interp_finite(interp1(layers(1).gps_time,layers(1).twtt,data.GPS_time));
+    surf = interp1(layers(1).gps_time,layers(1).twtt,data.GPS_time);
     surf_bins = round(interp1(data.Time,1:length(data.Time),surf));
-    bot = interp_finite(interp1(layers(2).gps_time,layers(2).twtt,data.GPS_time));
+    bot = interp1(layers(2).gps_time,layers(2).twtt,data.GPS_time);
     bot_bins = round(interp1(data.Time,1:length(data.Time),bot));
     dt = data.Time(2) - data.Time(1);
     switch param.layer_tracker.track{track_idx}.method
@@ -72,44 +82,61 @@ for track_idx = 1:length(param.layer_tracker.track)
         frame_fn_name = fullfile(filename,frm_dir,fname);
         track_data = load(frame_fn_name);
         for i = 1:16
-          num_isnan= 0;
-          num_points = 0;
-          surf = interp_finite(interp1(track_data.gps_time,track_data(i).twtt,data.GPS_time));
+          try
+          surf = interp1(track_data.gps_time,track_data.twtt(i,:),data.GPS_time);
+          catch ME
+            surf = interp_finite(interp1(track_data.gps_time,track_data.twtt(i,:),data.GPS_time),NaN);
+          end
           surf_bins_itr = round(interp1(data.Time,1:length(data.Time),surf));
-          num_gt_isfinite = length(find(isfinite(surf)));
-          for temp = 1:length(surf)
-            if(isfinite(surf(temp)) && isnan(surf_bins_itr(temp)))
-              num_isnan = num_isnan + 1;
-            end
-            if (abs(surf(temp)-surf_bins_itr(temp)) < 5*dt)
-              num_points = num_points+1;
-            end
-          end
-          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res(frm,i) = nanmean(abs(surf_bins - surf_bins_itr));
-          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_gt_isfinite = num_gt_isfinite;
-          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_isnan = num_isnan;
-          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_points = num_points;
+          num_gt_isfinite = sum(isfinite(surf_bins));
+          num_gt_isfinite_surf = num_gt_isfinite_surf + num_gt_isfinite;
+          num_isnan = sum(isfinite(surf_bins) & ~isfinite(surf_bins_itr));
+          num_isnan_surf = num_isnan_surf + num_isnan;
+          num_points = sum(abs(surf_bins-surf_bins_itr) < 5*dt);
+          num_points_surf = num_points_surf + num_points;
+%           for temp = 1:length(surf)
+%             if(isfinite(surf(temp)) && isnan(surf_bins_itr(temp)))
+%               num_isnan = num_isnan + 1;
+%             end
+%             if (abs(surf(temp)-surf_bins_itr(temp)) < 5*dt)
+%               num_points = num_points+1;
+%             end
+%           end
+          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res(frm_idx,param.layer_tracker.track{track_idx}.idx(i)) = nanmean(abs(surf_bins - surf_bins_itr));
+          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_gt_isfinite(frm_idx) = num_gt_isfinite;
+          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_isnan(frm_idx) = num_isnan;
+          res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_points(frm_idx) = num_points;
         end
         
-        
+        pos = 1;
         for i = 17:32
-          bot = interp_finite(interp1(track_data.gps_time,track_data(i).twtt,data.GPS_time));
-          bot_bins_itr = round(interp1(data.Time,1:length(data.Time),bot));
-          for temp = 1:length(bot)
-            if(isfinite(bot(temp)) && isnan(bot_bins_itr(temp)))
-              num_isnan = num_isnan + 1;
-            end
-            if (abs(bot(temp)-bot_bins_itr(temp)) < 5*dt)
-              num_points = num_points+1;
-            end
+          try
+          bot = interp1(track_data.gps_time,track_data.twtt(i,:),data.GPS_time);
+          catch ME
+          bot = interp_finite(interp1(track_data.gps_time,track_data.twtt(i,:),data.GPS_time),NaN);
           end
-          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res(frm,i) = nanmean(abs(bot_bins - bot_bins_itr));
+          bot_bins_itr = round(interp1(data.Time,1:length(data.Time),bot));
+          num_gt_isfinite = sum(isfinite(bot_bins));
+          num_gt_isfinite_bot = num_gt_isfinite_bot + num_gt_isfinite;
+          num_isnan = sum(isfinite(bot_bins) & ~isfinite(bot_bins_itr));
+          num_isnan_bot = num_isnan_bot + num_isnan;
+          num_points = sum(abs(bot_bins-bot_bins_itr) < 5*dt);
+          num_points_bot = num_points_bot + num_points;
+%           for temp = 1:length(bot)
+%             if(isfinite(bot(temp)) && isnan(bot_bins_itr(temp)))
+%               num_isnan = num_isnan + 1;
+%             end
+%             if (abs(bot(temp)-bot_bins_itr(temp)) < 5*dt)
+%               num_points = num_points+1;
+%             end
+         
+          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res(frm_idx,param.layer_tracker.track{track_idx}.idx(i)) = nanmean(abs(bot_bins - bot_bins_itr));
           
-          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_gt_isfinite = num_gt_isfinite;
-          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_isnan = num_isnan;
-          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_points = num_points;
+          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_gt_isfinite(frm_idx) = num_gt_isfinite;
+          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_isnan(frm_idx) = num_isnan;
+          res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).num_points(frm_idx) = num_points;
+          pos = pos+1;
         end
-        
       case 'viterbi'
         idx = 1;
         layer_params_bot(idx).name = sprintf('%s_%s_bottom',param.layer_tracker.track{track_idx}.name,param.layer_tracker.track{track_idx}.method);
@@ -132,12 +159,14 @@ for track_idx = 1:length(param.layer_tracker.track)
         layer_params_surf(idx).source = param.layer_tracker.layer_params.source;
         layer_params_surf(idx).layerdata_source = param.layer_tracker.layer_params.layerdata_source;
     end
+    ctr = ctr+1;
   end
-  res_matrix_surface = res_matrix_surface + res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res;
-  res_matrix_bottom = res_matrix_bottom + res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res;
+%   res_matrix_surface = res_matrix_surface + res_s.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res;
+%   res_matrix_bottom = res_matrix_bottom + res_b.(sprintf('%s_%s',param.layer_tracker.track{track_idx}.method,param.layer_tracker.track{track_idx}.name)).res;
 end
-
-
+[min_val_surf,min_idx1] = min(res_matrix_surface,[],1);
+[min_val_surf,min_idx2] = min(min_val_surf,[],2);
+min_idx1 = min_idx1(min_idx2);
 
 fprintf('hi');
 
