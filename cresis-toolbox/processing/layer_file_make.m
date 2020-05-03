2223716cd1a0e4e982107dae8a6cc461642859ee
John Paden <paden@ku.edu>
2020-05-03 10:17:58 -0500

    Some minor edits to layer making and the 2018 Antarctica Ground make_gps file

--- cresis-toolbox/processing/layer_file_make.m
+++ cresis-toolbox/processing/layer_file_make.m
@@ -1,13 +1,13 @@
-function make_layer_files(param,param_override)
-% make_layer_files(param,param_override)
+function layer_file_make(param,param_override)
+% layer_file_make(param,param_override)
 %
 % Makes layer files for the picker.m program. This should be run
-% from run_make_layer_files.m (that script sets up all the control
+% from run_layer_file_make.m (that script sets up all the control
 % variables).
 %
 % Author: John Paden
 %
-% See also: run_make_layer_files
+% See also: run_layer_file_make
 
 param = merge_structs(param,param_override);
 
@@ -18,8 +18,8 @@ fprintf('=====================================================================\n
 
 %% Input Checks
 
-if ~isfield(param.make_layer_files,'update_mode') || isempty(param.make_layer_files.update_mode)
-  param.make_layer_files.update_mode = 0;
+if ~isfield(param.layer_file_make,'update_mode') || isempty(param.layer_file_make.update_mode)
+  param.layer_file_make.update_mode = 0;
   % In all modes, if file does not exist, then a new file is created. If a
   % file exists, how it is updated is controlled by the update_mode field.
   % The update_mode field is a nonnegative integer scalar indicating:
@@ -37,29 +37,40 @@ if ~isfield(param.make_layer_files,'update_mode') || isempty(param.make_layer_fi
   % GPS time field.
 end
 
-if ~isfield(param.make_layer_files,'out_path') || isempty(param.make_layer_files.out_path)
-  param.make_layer_files.out_path = 'layer';
+if ~isfield(param.layer_file_make,'out_path') || isempty(param.layer_file_make.out_path)
+  param.layer_file_make.out_path = 'layer';
 end
 
 %% Make or update layer files
-layers = layerdata(param,param.make_layer_files.out_path);
-if param.make_layer_files.update_mode == 1
+layers = layerdata(param,param.layer_file_make.out_path);
+if param.layer_file_make.update_mode == 1
   if isempty(param.cmd.frms)
     layers.delete_all();
   else
     layers.delete_layer_file(param.cmd.frms);
   end
 end
-if param.make_layer_files.update_mode == 2
+if param.layer_file_make.update_mode == 2
   layers.update_gps_all();
 end
 layers.check_all();
-if isempty(layers.layer_organizer.lyr_name)
-  layer_organizer = [];
-  layer_organizer.lyr_name = {'surface','bottom'};
-  layer_organizer.lyr_group_name = {'standard','standard'};
+layer_names = layers.get_layer_names();
+surface_idx = find(strcmp('surface',layer_names));
+if isempty(surface_idx)
+  layer_organizer.lyr_name = {'surface'};
+  layer_organizer.lyr_group_name = {'standard'};
   layers.insert_layers(layer_organizer);
-  layers.update_layer(1:length(layers.frames.frame_idxs),'surface',layers.frames.gps_time([1 end]),[0 0]);
+  layers.update_layer(1:length(layers.frames.frame_idxs),'surface',layers.frames.gps_time([1 end]),[NaN NaN]);
+else
+  obj.layer_organizer.lyr_group_name{surface_idx} = 'standard';
+end
+bottom_idx = find(strcmp('bottom',layer_names));
+if isempty(bottom_idx)
+  layer_organizer.lyr_name = {'bottom'};
+  layer_organizer.lyr_group_name = {'standard'};
+  layers.insert_layers(layer_organizer);
   layers.update_layer(1:length(layers.frames.frame_idxs),'bottom',layers.frames.gps_time([1 end]),[NaN NaN]);
+else
+  obj.layer_organizer.lyr_group_name{bottom_idx} = 'standard';
 end
 layers.save();
