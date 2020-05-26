ba045dfd28b1b898d6b52824e90d097ed1d3476e
John Paden <paden@ku.edu>
2020-05-26 08:16:01 -0500

    surfdata: fix v3 check hack >1, create_configs: add ping pong, layer_tracker_2D: white space only changes

--- cresis-toolbox/missions/create_configs_2019_Antarctica_Ground_rds.m
+++ cresis-toolbox/missions/create_configs_2019_Antarctica_Ground_rds.m
@@ -137,3 +137,73 @@ for freq_idx = [1]
   write_radar_config(config);
   config = old_config;
 end
+
+%% Ping Pong Mode
+% <4500 m thick ice
+ice_thickness = [4500];
+for freq_idx = [1]
+  param = default_radar_params_2019_Antarctica_Ground_rds;
+  config = param.config;
+  
+  config.flight_hours = 10;
+  config.prf = prf;
+  
+  config.zeropimods = [0 180]; % 180 causes a special mode in some ADC/DAC
+  
+  config.tg.staged_recording = {[1 2 3 3]}; % HERE
+  config.tg.altitude_guard = 100*12*2.54/100;
+  config.tg.Haltitude = 100*12*2.54/100;
+  config.tg.Hice_thick = ice_thickness(freq_idx);
+  
+  config.create_IQ = false;
+  
+  config.f0 = f0_list(freq_idx);
+  config.f1 = f1_list(freq_idx);
+  config.DDC_freq = 160e6;
+  config.zeropimods = [0 180];
+  
+  config.wfs(1).presums = 128; % HERE
+  config.wfs(2).presums = 128; % HERE
+  config.wfs(3).presums = 256;
+  config.wfs(4).presums = 256; % HERE
+  config.tukey = 0.08;
+  config.wfs(1).Tpd = 0.5e-6;
+  config.wfs(2).Tpd = 3e-6;
+  config.wfs(3).Tpd = 10e-6;
+  config.wfs(4).Tpd = 10e-6; % HERE
+  config.wfs(1).tukey = 0.1;
+  config.wfs(2).tukey = 0.1;
+  config.wfs(3).tukey = 0.1;
+  config.wfs(4).tukey = 0.1; % HERE
+  
+  config.wfs(1).name = '0p5us_high';
+  config.wfs(2).name = '3us_high';
+  config.wfs(3).name = '10us_lefttx'; % HERE
+  config.wfs(4).name = '10us_righttx'; % HERE
+  
+  config.wfs(1).tx_weights = final_DDS_amp{cal_settings_list(freq_idx)}; % HERE
+  config.wfs(1).tx_enable = [1 1 1 1]; % HERE
+  config.wfs(2).tx_weights = final_DDS_amp{cal_settings_list(freq_idx)}; % HERE
+  config.wfs(2).tx_enable = [1 1 1 1]; % HERE
+  config.wfs(3).tx_weights = final_DDS_amp{cal_settings_list(freq_idx)}; % HERE
+  config.wfs(3).tx_weights([2 4]) = 0; % HERE
+  config.wfs(3).tx_enable = [1 0 1 0]; % HERE
+  config.wfs(4).tx_weights = final_DDS_amp{cal_settings_list(freq_idx)}; % HERE
+  config.wfs(4).tx_weights([1 3]) = 0; % HERE
+  config.wfs(4).tx_enable = [0 1 0 1]; % HERE
+  
+  config.wfs(1).phase = final_DDS_phase{cal_settings_list(freq_idx)};
+  config.wfs(2).phase = final_DDS_phase{cal_settings_list(freq_idx)};
+  config.wfs(3).phase = final_DDS_phase{cal_settings_list(freq_idx)};
+  config.wfs(4).phase = final_DDS_phase{cal_settings_list(freq_idx)};
+  
+  config.wfs(1).delay = final_DDS_time{cal_settings_list(freq_idx)};
+  config.wfs(2).delay = final_DDS_time{cal_settings_list(freq_idx)};
+  config.wfs(3).delay = final_DDS_time{cal_settings_list(freq_idx)};
+  config.wfs(4).delay = final_DDS_time{cal_settings_list(freq_idx)};
+  
+  config.arena.psc_name = sprintf('pingpong_%.0f-%.0fMHz_%.0fft_%.0fus_%.0fmthick', ...
+    config.f0/1e6,config.f1/1e6,config.tg.Haltitude*100/12/2.54,config.wfs(end).Tpd*1e6,config.tg.Hice_thick); % HERE
+  config.arena.fn = fullfile(arena_base_dir,sprintf('%s.xml',config.arena.psc_name));
+  write_radar_config(config);
+end
