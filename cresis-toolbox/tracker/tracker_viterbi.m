6844efb89971c658049f4c2e864f270ab6876fa4
Reece Mathews <mathewsreece@gmail.com>
2020-05-26 15:43:56 -0500

    Pare tracker_viterbi down to necessary params and actually use viterbi2

--- cresis-toolbox/tracker/tracker_viterbi.m
+++ cresis-toolbox/tracker/tracker_viterbi.m
@@ -3,6 +3,7 @@ function labels = tracker_viterbi(data,track)
 %
 
 gt = track.crossovers;
+physical_constants;
 
 try
   transition_weight = track.viterbi.transition_weight;
@@ -15,30 +16,11 @@ catch ME
   gt_cutoff = 50;
 end
 
-elevation = param.echowin.eg.elev;
-vel_air = c/2;
-vel_ice = c/(sqrt(er_ice)*2);
-dt = param.echo_time(2) - param.echo_time(1);
-along_track_slope = diff(elevation);
+along_track_slope = zeros(1, size(data, 2) - 1);
 
-yaxis_choice = get(param.echowin.left_panel.yaxisPM,'Value');
-if yaxis_choice == 1 % TWTT
-  drange = dt * vel_air;
-elseif yaxis_choice == 2 % WGS_84 Elevation
-  drange = dt * vel_ice;
-elseif yaxis_choice == 3 % Range
-  drange = dt * vel_ice;
-elseif yaxis_choice == 4 % Range bin
-  drange = dt * vel_air;
-elseif yaxis_choice == 5 % Surface flat
-  drange = dt * vel_ice;
-end
-along_track_slope = round(along_track_slope / drange);
-
-%% Call viterbi.cpp
 upper_bounds = nan(1, size(data, 2));
 lower_bounds = nan(1, size(data, 2));
 upper_bounds(gt(1, :)) = gt(2, :) - gt_cutoff;
 lower_bounds(gt(1, :)) = gt(2, :) + gt_cutoff;
 
-labels = tomo.viterbi(double(data), along_track_slope, transition_weight, upper_bounds, lower_bounds);
+labels = tomo.viterbi2(double(data), along_track_slope, transition_weight, upper_bounds, lower_bounds);
