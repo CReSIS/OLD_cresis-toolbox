2b5f275d7f3417deae02e65b6b8cf248f4074f5c
Reece Mathews <mathewsreece@gmail.com>
2020-06-01 16:18:59 -0500

    Update vertical bound handling for tracker_viterbi

--- cresis-toolbox/tracker/tracker_viterbi.m
+++ cresis-toolbox/tracker/tracker_viterbi.m
@@ -9,26 +9,26 @@ try
   transition_weight = track.viterbi.transition_weight;
 catch ME
   transition_weight = 1;
-  warning('transition_weight not specified or invalid. Defaulting to %.2f.', transition_weight);
+  warning('transition_weight not specified or invalid. Defaulting to %.2f.', ...
+          transition_weight);
 end
 try
   gt_cutoff = track.viterbi.gt_cutoff;
 catch ME
   gt_cutoff = 50;
-  warning('gt_cutoff not specified or invalid. Defaulting to %.0f.', gt_cutoff);
+  warning('gt_cutoff not specified or invalid. Defaulting to %.0f.', ...
+          gt_cutoff);
 end
 
 along_track_slope = zeros(1, size(data, 2) - 1);
 
-upper_bounds = nan(1, size(data, 2));
-lower_bounds = nan(1, size(data, 2));
+upper_bounds = track.min_bin;
+lower_bounds = track.max_bin;
 upper_bounds(gt(1, :)) = gt(2, :) - gt_cutoff;
 lower_bounds(gt(1, :)) = gt(2, :) + gt_cutoff;
 
-upper_bounds(isnan(upper_bounds) | upper_bounds < 1) = 1;
-lower_bounds(lower_bounds < 1) = 1;
-lower_bounds(isnan(lower_bounds)) = size(data, 1);
+upper_bounds(upper_bounds < 1) = 1;
+lower_bounds(lower_bounds > size(data, 1)) = size(data, 1);
 
-% TODO[reece]: Probably allow surface to be passed and restrict upper_bound accordingly
-
-labels = tomo.viterbi2(single(data), along_track_slope, transition_weight, upper_bounds, lower_bounds);
+labels = tomo.viterbi2(single(data), along_track_slope, transition_weight, ...
+                       upper_bounds, lower_bounds);
