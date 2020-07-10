e88fb3a225ccf613832615eb425ee5721b40ba71
Reece Mathews <mathewsreece@gmail.com>
2020-07-10 11:55:30 -0500

    Add documentation to trws2_CT_image_test

--- cresis-toolbox/+tomo/trws2_CT_image_test.m
+++ cresis-toolbox/+tomo/trws2_CT_image_test.m
@@ -11,17 +11,20 @@ Et  = NaN;
 Esv = NaN;
 Ex  = NaN;
 
-% Num sampled points
-Nt  = 100;
-Nsv = 100;
-Nx  = 100;
+% Num sampled points (resolutions)
+Nt  = 10;
+Nsv = 10;
+Nx  = 10;
 
-MAX_LOOPS = 2;
-INDEX_EVEN_LOOP = true;
-PLOT_POINTS = false;
-PLOT_INDICES = false;
-PLOT_THRESHOLD = 7;
+MAX_LOOPS = 2; % Number of iterations of TRWS to perform
+INDEX_EVEN_LOOP = true; % Plot index traversal order for an even loop
+PLOT_POINTS = false; % Plot data points as sphere's
+PLOT_INDICES = false; % Plot indices on which dt is performed
+PLOT_THRESHOLD = 7; % The minimum intensity to plot if PLOT_POINTS is true
+RELOAD_DATA = true; % When false, only load trws_data if not already loaded
 
+
+%% Load Data
 param.radar_name = 'rds';
 surfdata_source = 'surfData_paden';
 
@@ -31,19 +34,35 @@ if 0
     out_type = 'music3D_paden';
     frm = 1;
     echogram_fn = fullfile(ct_filename_out(param,out_type,''),sprintf('Data_%s_%03d.mat',param.day_seg,frm));
+    FIND_SURF = true;
 elseif 1
+    correct_surface = load('C:/Users/mathe/Documents/MATLAB/TRWS_CT results/perm Antarctica/entire_matrix_surf.mat');
+    correct_surface = correct_surface.correct_surface;
+    FIND_SURF = false;
+    Nt  = NaN;
+    Nsv = NaN;
+    Nx  = NaN;
+    Esv = 64;
+elseif 0
     param.season_name = '2014_Greenland_P3';
     param.day_seg = '';
     out_type = 'multipass';
     frm = 4;
     echogram_fn = fullfile(ct_filename_out(param,out_type,''), 'summit_2012_2014_allwf_2012_music.mat');
+    FIND_SURF = true;
 end
 
-if ~exist('mdata', 'var')
-  mdata = load(echogram_fn);
+if FIND_SURF
+    if ~exist('mdata', 'var') || RELOAD_DATA
+      mdata = load(echogram_fn);
+    end
+    trws_data = 10*log10(mdata.Tomo.img);
 end
-trws_data = 10*log10(mdata.Tomo.img);
 
+
+%% Create length vars
+
+% Set default start index
 if ~exist('St', 'var') || isnan(St) || isempty(St)
     St  = 1;
 end
@@ -54,16 +73,29 @@ if ~exist('Sx', 'var') || isnan(Sx) || isempty(Sx)
     Sx  = 1;
 end
 
+% Set default end indices
 if ~exist('Et', 'var') || isnan(Et) || isempty(Et)
-    Et  = size(trws_data, 1);
+    if FIND_SURF
+        Et = size(trws_data, 1);
+    else
+        Et = size(correct_surface, 1); 
+    end
 end
 if ~exist('Esv', 'var') || isnan(Esv) || isempty(Esv)
+    if ~FIND_SURF
+        error('Cannot automatically determine Esv without loading echo_gram.');
+    end
     Esv = size(trws_data, 2);
 end
 if ~exist('Ex', 'var') || isnan(Ex) || isempty(Ex)
-    Ex  = size(trws_data, 3);
+    if FIND_SURF
+        Ex = size(trws_data, 3);
+    else
+        Ex = size(correct_surface, 2);
+    end
 end
 
+% Set default resolutions
 if ~exist('Nt', 'var') || isnan(Nt) || isempty(Nt)
     Nt  = Et;
 end
@@ -74,7 +106,7 @@ if ~exist('Nx', 'var') || isnan(Nx) || isempty(Nx)
     Nx  = Ex;
 end
 
-% Step size
+% Calculate step size
 Tt  = round((Et-St)/Nt);
 Tsv = round((Esv-Ssv)/Nsv);
 Tx  = round((Ex-Sx)/Nx);
@@ -83,22 +115,34 @@ Tt  = max(Tt, 1);
 Tsv = max(Tsv, 1);
 Tx  = max(Tx, 1);
 
-trws_data = trws_data(St:Tt:Et, Ssv:Tsv:Esv, Sx:Tx:Ex);
-Nt  = size(trws_data, 1);
-Nsv = size(trws_data, 2);
-Nx  = size(trws_data, 3);
+% Resample and window data to given start and end indices and resolutions
+if FIND_SURF
+    trws_data = trws_data(St:Tt:Et, Ssv:Tsv:Esv, Sx:Tx:Ex);
+    % Normalize data
+    trws_data = trws_data - min(trws_data(:));
+    trws_data = trws_data/max(trws_data(:)) * 10;
+end
 
-trws_data = trws_data - min(trws_data(:));
-trws_data = trws_data/max(trws_data(:)) * 10;
+% Correct num sampled points to match step size constraint
+Nt  = length(St:Tt:Et);
+Nsv = length(Ssv:Tsv:Esv);
+Nx  = length(Sx:Tx:Ex);
 
+% Create default slopes, weights, and bounds
 at_slope  = zeros(1, Nx);
 at_weight = 1;
-bounds = ones(2, Nx);
-bounds(2, :) = Nsv;
-correct_surface = tomo.trws2_CT_perm(single(trws_data),single(at_slope), ...
-  single(at_weight), uint32(MAX_LOOPS), uint32(bounds-1));
+CT_bounds = ones(2, Nx);
+CT_bounds(2, :) = Nsv;
 
-save([SAVE_PATH 'entire_matrix_surf.mat'], 'correct_surface');
+top_bounds = ones(Nsv, Nx) * 5;
+bottom_bounds = ones(Nsv, Nx) * Nt - 5;
+
+% Perform TRWS and save surface
+if FIND_SURF
+  correct_surface = tomo.trws2_CT_perm(single(trws_data),single(at_slope), ...
+    single(at_weight), uint32(MAX_LOOPS), uint32(CT_bounds-1), top_bounds, bottom_bounds);
+  save([SAVE_PATH 'entire_matrix_surf.mat'], 'correct_surface');
+end
 
 figure(FIGURE_NUM);
 clf;
@@ -112,8 +156,8 @@ clear surf;
 surf(correct_surface, 'FaceAlpha', .6, 'FaceColor', 'interp', 'LineStyle', 'none');
 blue   = [86 , 135, 214]./255;
 orange = [232, 145, 90 ]./255;
-surf(X, Z, repmat(bounds(1, :), Nt, 1), 'FaceColor', blue, 'LineStyle', 'none', 'FaceAlpha', 0.1);
-surf(X, Z, repmat(bounds(2, :), Nt, 1), 'FaceColor', orange, 'LineStyle', 'none', 'FaceAlpha', 0.1);
+surf(X, Z, repmat(CT_bounds(1, :), Nt, 1), 'FaceColor', blue, 'LineStyle', 'none', 'FaceAlpha', 0.1);
+surf(X, Z, repmat(CT_bounds(2, :), Nt, 1), 'FaceColor', orange, 'LineStyle', 'none', 'FaceAlpha', 0.1);
 xlim([1 Nx]);
 %     xticks(1:Nx);
 xlabel('X : Along-Track (Nx, DIM 2)');
@@ -170,14 +214,8 @@ if PLOT_POINTS || PLOT_INDICES
                 end
               end
 
-              msg_idx = NaN;
-
-              if h_idx == 1
-                  msg_idx = d+w*Nt + 1;
-              end
-
-              if ~isnan(msg_idx) && PLOT_INDICES
-                  text(w_idx, d_idx, h_idx, sprintf('%d', msg_idx));
+              if h_idx == 1 && PLOT_INDICES
+                  text(w_idx, d_idx, h_idx, sprintf('%d', d+w*Nt + 1));
               end
           end
       end
