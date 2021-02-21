f630a58f3e4c3966aeab7ffbe2fd141487b87f58
ts9 <theresa.moore@ku.edu>
2021-02-21 09:10:32 -0600

    update collate_snapshots to remove scr threshold

--- cresis-toolbox/processing/collate_snapshots.m
+++ cresis-toolbox/processing/collate_snapshots.m
@@ -1,12 +1,14 @@
 function collate_snapshots(param,param_override)
 % collate_snapshots(param,param_override)
+%
 % 
 % THIS FUNCTION CULLS SNAPSHOTS CONTAINING SINGLE TARGETS - FIRST STEP OF
 % LUT GENERATION
 %
 % Function that takes snapshots, pulls out calibration pixels and stores
 % with corresponding surface ARRIVAL ANGLES (not surface intersection
-% angles, i.e. roll-corrected)
+% angles, i.e. roll-corrected).  Currently culls by number of targets and
+% signal to clutter ratio.
 %
 % TO DO:  
 %   1. ADD FUNCTIONALITY TO STORE MASK IN A MEANINGFULL WAY FOR BROWSING
@@ -92,7 +94,7 @@ if ~isfield(param.collate_snapshots,'same_doa_guard') || isempty(param.collate_s
   param.collate_snapshots.same_doa_guard = 5; % degrees
 end
 
-%%
+%% collate_snapshots: load support files and loop on frames
 % Load frames file
 frames = frames_load(param);
 
@@ -126,7 +128,7 @@ for frm_idx = 1:length(param.cmd.frms)
   end
   
   
-  % Loop over images in the list
+  %% collate_snapthost: loop over images in the list
   for img_idx = 1:length(param.collate_snapshots.img_list)
     % Initialize variables
     Tomo = [];
@@ -159,25 +161,36 @@ for frm_idx = 1:length(param.cmd.frms)
       load(fn,'Tomo','Time','Elevation','GPS_time','Heading','Latitude','Longitude','Pitch','Roll','param_array','param_records','param_sar','file_version');
       [Nt, Nc, Nx]  = size(Tomo.img);
       
-      % Reformat data
+      %%  collate_snapshots: reformat raw snapshot outputs
       Tomo.img = permute(Tomo.img,[2 1 3]);
       Tomo.power = permute(Tomo.power,[2 1 3]);
       Tomo.surf_theta = permute(Tomo.surf_theta,[2 1 3]);
       Tomo.surf_ice_mask = permute(Tomo.surf_ice_mask,[2 1 3]);
       Tomo.two_src_mask = squeeze(sum(~isnan(Tomo.surf_theta),1))==2;
+      Tomo.one_src_mask = squeeze(sum(~isnan(Tomo.surf_theta),1))==1;      
       Tomo.surf_ice_mask = squeeze(any(Tomo.surf_ice_mask,1));
       Tomo.surf_ice_mask = cumsum(Tomo.surf_ice_mask,1) >= 1;
       Tomo.surf_multiple_mask = true(size(Tomo.surf_ice_mask));
+      Tomo.Nsrc = squeeze(sum(~isnan(Tomo.surf_theta)));
+      Tomo.edge_mask = false(size(Tomo.Nsrc));
+      %% collate_snapshots: create pixel culling 
       
-      % Interpolate surface onto snapshot slow time axis
+      % Surface multiple mask
       if ~isempty(layer)
         surface = interp_finite(interp1(layer.gps_time,layer.twtt,GPS_time));
+        multiple = 2*surface;
         for rline_idx = 1:Nx
-          surf_bin = round(interp_finite(interp1(Time,1:Nt,surface(rline_idx))));
-          surf_bins = surf_bin + [-param.collate_snapshots.multiple_guard_bins:param.collate_snapshots.multiple_guard_bins];
-          if min(surf_bins) > 1 & max(surf_bins) < Nt
-            Tomo.surf_multiple_mask(surf_bins,rline_idx) = false;
+%           surf_bin = round(interp_finite(interp1(Time,1:Nt,surface(rline_idx))));
+%           surf_bins = surf_bin + [-param.collate_snapshots.multiple_guard_bins:param.collate_snapshots.multiple_guard_bins];
+          multiple_bin = round(interp_finite(interp1(Time,1:Nt,multiple(rline_idx))));
+          multiple_bins = multiple_bin + [-param.collate_snapshots.multiple_guard_bins:param.collate_snapshots.multiple_guard_bins];
+          if min(multiple_bins) > 1 & max(multiple_bins) < Nt
+            Tomo.surf_multiple_mask(multiple_bins,rline_idx) = false;
           end
+% 
+%           if min(surf_bins) > 1 & max(surf_bins) < Nt
+%             Tomo.surf_multiple_mask(surf_bins,rline_idx) = false;
+%           end
         end
       end
       
@@ -186,49 +199,59 @@ for frm_idx = 1:length(param.cmd.frms)
       
       % Sort powers in descending order
       [Tomo.power,sort_idxs] = sort(Tomo.power,1,'descend');
-      Tomo.power = 10*log10(abs(Tomo.power));
-      
-      
+%       Tomo.power = 10*log10(abs(Tomo.power));
+
       % Convert surface angle to DOA
       Tomo.surf_doa = bsxfun(@minus,Tomo.surf_theta,permute(Roll*180/pi,[1 3 2]));
       
       % Maximum number of sources
       Ndoa_max = size(Tomo.surf_doa,1);
+      
+      % Sort indexes for the sources
       doa_sort_idxs = bsxfun(@plus,bsxfun(@plus,sort_idxs,Ndoa_max*(0:Nt-1)),permute(Ndoa_max*Nt*(0:Nx-1),[1 3 2]));
       
-      % For more than one source, threshold highestst
-      if Ndoa_max > 1
-        % Threshold
-        Tomo.power_mask = squeeze(Tomo.power(1,:,:) > Tomo.power(2,:,:) + param.collate_snapshots.scr_threshold);
-      else
-        Tomo.power_mask = true(size(squeeze(Tomo.power)));
-      end
-      
-      Tomo.surf_theta = Tomo.surf_theta(doa_sort_idxs);
-      
+      % Source incidence angles of clutter and calibration angles      
+      Tomo.surf_theta = Tomo.surf_theta(doa_sort_idxs);      
       Tomo.surf_doa   = Tomo.surf_doa(doa_sort_idxs);
+      
+      Tomo.backlobe_mask = squeeze(sum(abs(Tomo.surf_doa) > 90,1) >=1);
+
+      % Same doa mask
       Tomo.same_doa_mask = squeeze(abs(Tomo.surf_doa(1,:,:)-Tomo.surf_doa(2,:,:)) < param.collate_snapshots.same_doa_guard);
       
-      Tomo.mask = Tomo.two_src_mask & ~Tomo.surf_ice_mask & (Tomo.power_mask | Tomo.same_doa_mask) & Tomo.surf_multiple_mask;
+      % Fast time mask
+      Tpd = param.radar.wfs(img).Tpd;
+      Tomo.edge_mask = repmat(Time >= Time(end) - Tpd/2,1,Nx);
+  
+      % Final culling mask      
+%       Tomo.mask = Tomo.two_src_mask & ~Tomo.surf_ice_mask & (Tomo.power_mask | Tomo.same_doa_mask) & Tomo.surf_multiple_mask;
+%       Tomo.mask = Tomo.two_src_mask & ~Tomo.surf_ice_mask & Tomo.power_mask & ~Tomo.same_doa_mask & Tomo.surf_multiple_mask;
+%       Tomo.mask = (Tomo.one_src_mask | Tomo.two_src_mask) & ~Tomo.surf_ice_mask &  ~Tomo.same_doa_mask & Tomo.surf_multiple_mask & ~Tomo.backlobe_mask;
+            Tomo.mask = (Tomo.one_src_mask | Tomo.two_src_mask) & ~Tomo.surf_ice_mask &  ~Tomo.same_doa_mask & Tomo.surf_multiple_mask & ~Tomo.edge_mask;
+      % If only one source (not likely) store nans
+      if Ndoa_max == 1         
+        Tomo.power(2,:,:) = nan(size(Tomo.mask));
+        Tomo.surf_doa(2,:,:) = nan(size(Tomo.mask));
+        Tomo.surf_theta(2,:,:) = nan(size(Tomo.mask));
+      end
       
-      
-      %% Create simplified output
+      Nsrc_max = min([2 Ndoa_max]);
+           
+      %% colate_snapshots: create simplified output
       
       % Create a list of the rxs
       wf = param_array.array_proc.imgs{1}(1,1);
       adc_list = param_array.array_proc.imgs{1}(:,2);
       rx_list = param.radar.wfs(wf).rx_paths(adc_list);
       
-      
-      % Pick out the snapshots of the good pixels
-      snapshots.sv_list = Tomo.img(:,Tomo.mask);
-      
-      % Pick out the good surface intersections and doas
-      snapshots.surf_doa = Tomo.surf_doa(1,Tomo.mask);
-      snapshots.surf_theta = Tomo.surf_theta(1,Tomo.mask);
-      
-      % Pick out corresponding power
-      snapshots.power = Tomo.power(1,Tomo.mask);
+     
+      % Pick out the snapshots, power, surface incidence angles, cal doas,
+      % power of good pixels
+      snapshots.sv_list = Tomo.img(:,Tomo.mask);     
+      snapshots.surf_doas = Tomo.surf_doa(1:Nsrc_max,Tomo.mask);
+      snapshots.surf_thetas = Tomo.surf_theta(1:Nsrc_max,Tomo.mask);
+      snapshots.power = Tomo.power(1:Nsrc_max,Tomo.mask);   
+      snapshots.Nsrc = Tomo.Nsrc(Tomo.mask).';
       
       % Pick out gps time and twtt of the culled pixels
       twtt_matrix     = single(repmat(Time,1,Nx));
@@ -248,7 +271,7 @@ for frm_idx = 1:length(param.cmd.frms)
       snapshots.pitch = pitch_matrix(Tomo.mask);
       snapshots.heading = heading_matrix(Tomo.mask);
       snapshots.roll = roll_matrix(Tomo.mask);
-      
+     
       snapshots.twtt = snapshots.twtt(:).';
       snapshots.gps_time = snapshots.gps_time(:).';
       snapshots.elev = snapshots.elev(:).';
@@ -279,6 +302,15 @@ for frm_idx = 1:length(param.cmd.frms)
   end
 end
 
-
+     % For more than one source, threshold highestst
+%       if Ndoa_max > 1
+%         % Threshold
+%         Tomo.power_mask = squeeze(Tomo.power(1,:,:) > Tomo.power(2,:,:) + param.collate_snapshots.scr_threshold);
+% %         Tomo.scr = nan(size(Tomo.power_mask));
+% %         Tomo.scr = squeeze(Tomo.power(1,:,:) - Tomo.power(2,:,:));
+%       else
+%         Tomo.power_mask = true(size(squeeze(Tomo.power)));
+% %         Tomo.scr = nan(size(Tomo.power_mask));
+%       end      
 
 
