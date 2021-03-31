8fdfef57492a772244753af5d06ec78d37c6dded
jiluli <jiluli@ku.edu>
2021-03-31 10:23:54 -0500

    add L1B or L2 options for checking nsidc files.

--- cresis-toolbox/posting/run_check_nsidc_files.m
+++ cresis-toolbox/posting/run_check_nsidc_files.m
@@ -7,13 +7,14 @@
 %% User Setup
 % =====================================================================
 param_override = [];
-params = read_param_xls(ct_filename_param('snow_param_2018_Antarctica_DC8.xls'));
+params = read_param_xls(ct_filename_param('rds_param_2018_Greenland_P3.xls'));
 
 % Example to run a specific segment and frame by overriding parameter spreadsheet values
 % params = ct_set_params(params,'cmd.generic',0);
 % params = ct_set_params(params,'cmd.generic',1,'day_seg','20181027_02');
-param_override.nsidc_file_folder = '/cresis/snfs1/scratch/jliwestc/nsidc/2018_Antarctica_DC8/snow/IRSNO1B_Files/Data_2018_AN';
-param_override.nsidc_SP_file_folder = '/cresis/snfs1/scratch/jliwestc/nsidc/2018_Antarctica_DC8/snow/IRSNO1B_Files/Spatial_Premet_Files';
+param_override.nsidc_file_folder = '/cresis/snfs1/scratch/jliwestc/nsidc/2018_Greenland_P3/rds/IRMCR1B_Files/Data_2018_GR';
+param_override.nsidc_SP_file_folder = '/cresis/snfs1/scratch/jliwestc/nsidc/2018_Greenland_P3/rds/IRMCR1B_Files/Spatial_Premet_Files';
+param_override.nsidc_file_type = 'L1B'; % L1B or L2
 
 %% Automated Section
 % =====================================================================
@@ -33,18 +34,20 @@ all_supplement_file_num = 0;
 all_SP_file_num = 0;
 all_map_file_num = 0;
 all_img_file_num = 0;
+all_csv_file_num = 0;
 for param_idx = 1:length(params)
   param = params(param_idx);
   if ~isfield(param.cmd,'generic') || iscell(param.cmd.generic) || ischar(param.cmd.generic) || ~param.cmd.generic
     continue;
   end
-  [total_num,nc_num,supplement_num,SP_num,map_num,img_num] = check_nsidc_files(param,param_override);
+  [total_num,nc_num,supplement_num,SP_num,map_num,img_num,csv_num] = check_nsidc_files(param,param_override);
   all_file_num = all_file_num + total_num;
   all_nc_file_num = all_nc_file_num + nc_num;
   all_supplement_file_num = all_supplement_file_num + supplement_num;
   all_SP_file_num = all_SP_file_num + SP_num;
   all_map_file_num = all_map_file_num + map_num;
   all_img_file_num = all_img_file_num + img_num;
+  all_csv_file_num = all_csv_file_num + csv_num;
 end
 sprintf('number of files to deliver = %d\nnumber of nc files = %d\nnumber of supplement files = %d\nnumber of Spatial and Premet files = %d\nnumber of frames or map files = %d\nnumber of echogram images = %d\n',...
   all_file_num, all_nc_file_num,all_supplement_file_num,all_SP_file_num,all_map_file_num, all_img_file_num)
