58a8303bcfb5bf6a46a395978fc1e0f571908f55
Reece Mathews <mathewsreece@gmail.com>
2021-12-08 04:02:25 -0600

    Create matlab script to calculate crossovers

---
+++ cresis-toolbox/OPS-MATLAB/utility/opsCalculateCrossovers.m
@@ -0,0 +1,47 @@
+function [status,data] = opsCalculateCrossovers()
+%
+% [status,data] = opsCalculateCrossovers()
+%
+% Retrieves segments which have not had crossovers calculated and
+% calculates crossovers on them.
+%
+% Input:
+%   none
+%
+% Output:
+%   status: integer (0:Error,1:Success,2:Warning)
+%
+% Author: Reece Mathews
+
+[authParam,~,~] = opsAuthenticate(struct('properties',[]));
+
+% SEND THE COMMAND TO THE SERVER
+opsCmd;
+sys = 'rds';
+
+query = sprintf('SELECT id from %s_segments where crossover_calc=false;', sys);
+[~,data] = opsQuery(query);
+segments = [data{:}];
+jsonStruct = struct('segments',segments);
+
+% CONVERT THE JSON STRUCTURE TO A JSON STRING
+try
+    jsonStr = tojson(jsonStruct);
+catch ME
+    jsonStr = savejson('',jsonStruct,'FloatFormat','%d','NaN','null');
+end
+
+% SEND THE COMMAND TO THE SERVER
+opsCmd;
+if gOps.profileCmd
+    [jsonResponse,~] = opsUrlRead(strcat(gOps.serverUrl,'profile'),gOps.dbUser,gOps.dbPswd,...
+        'Post',{'app' sys 'data' jsonStr 'view' 'calculateCrossovers'});
+else
+    [jsonResponse,~] = opsUrlRead(strcat(gOps.serverUrl,'calculateCrossovers'),gOps.dbUser,gOps.dbPswd,...
+        'Post',{'app' sys 'data' jsonStr});
+end
+
+% DECODE THE SERVER RESPONSE
+[status,message] = jsonResponseDecode(jsonResponse);
+
+end
