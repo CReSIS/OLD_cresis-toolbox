b1ce090b37c1a957d139e2217d2adc3ee51871cb
Reece Mathews <mathewsreece@gmail.com>
2022-06-20 12:46:34 -0500

    Add functions to use simplifySegmentsResolution view

---
+++ cresis-toolbox/OPS-MATLAB/input/opsSimplifySegmentsResolution.m
@@ -0,0 +1,45 @@
+function [status,data] = opsSimplifySegmentsResolution(sys,param)
+%
+% [status,message] = opsSimplifySegmentsResolution(sys,param)
+%
+% Simplify segments' resolution using ST_SimplifyPreserveTopology
+%
+% Input:
+%   sys: (string) sys name ('rds','accum','snow',...)
+%   param: structure with fields
+%     properties.segment: {string(s)} OR segment_id [integer(s)]
+%     properties.season: (string) the season name of the segment(s) to be altered.
+%     properties.resolution: number of meters between each point
+%
+% Output:
+%   status: integer (0:Error,1:Success,2:Warning)
+%   data: status message
+%
+% Author: Reece Mathews, Trey Stafford
+
+% CONSTRUCT THE JSON STRUCTURE
+[param,~,~] = opsAuthenticate(param);
+jsonStruct = struct('properties',param.properties);
+
+% CONVERT THE JSON STRUCUTRE TO A JSON STRING
+try
+  jsonStr = tojson(jsonStruct);
+catch ME
+  jsonStr = savejson('',jsonStruct,'FloatFormat','%2.10f','NaN','null');
+end
+
+% SEND THE COMMAND TO THE SERVER
+opsCmd;
+if gOps.profileCmd
+  [jsonResponse,~] = opsUrlRead(strcat(gOps.serverUrl,'profile'),gOps.dbUser,gOps.dbPswd,...
+    'Post',{'app' sys 'data' jsonStr 'view' 'simplifySegmentsResolution'});
+else
+  [jsonResponse,~] = opsUrlRead(strcat(gOps.serverUrl,'alter/path/simplify'),gOps.dbUser,gOps.dbPswd,...
+    'Post',{'app' sys 'data' jsonStr});
+end
+
+% DECODE THE SERVER RESPONSE
+[status,data] = jsonResponseDecode(jsonResponse);
+
+end
+
