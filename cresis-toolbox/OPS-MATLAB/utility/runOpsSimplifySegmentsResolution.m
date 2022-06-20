cc414ab2f58ccb4148a5f9c3e369fff91f5d9db1
Reece Mathews <mathewsreece@gmail.com>
2022-06-20 12:46:34 -0500

    Add functions to use simplifySegmentsResolution view

---
+++ cresis-toolbox/OPS-MATLAB/utility/runOpsSimplifySegmentsResolution.m
@@ -0,0 +1,67 @@
+% =========================================================================
+% OPS BULK SIMPLIFY SEGMENTS RESOLUTION
+%
+% Changes the path resolution of segments in the database using ST_SimplifyPreserveTopology
+%
+% To use this script manually edit the input fileds under USER INPUT.
+%
+% Data inputs required:
+%   paramFn: param sheet. Generic column flags segments to alter. 
+%   sysName: System ('rds','accum', 'kuband', 'snow')
+%   resolution: the along-track path resolution (meters)
+%
+
+% Authors: Trey Stafford
+%
+% see also opsAlterPathResolution.m
+%
+% =========================================================================
+
+%% USER INPUT
+% ----------------------------------------------------------------
+% paramFn: FILENAME.xls OF EXCEL CReSIS PARAMS SHEET
+paramFn = 'rds_param_2012_Greenland_P3.xls';
+
+% ----------------------------------------------------------------
+% sysName: SYSTEM NAME ('rds','snow','accum','kuband')
+sysName = 'rds';
+
+% ----------------------------------------------------------------
+% resolution: the resolution in meters to which to simplify the segments using the Postgis
+% command, ST_SimplifyPreserveTopology. This removes any points which can be removed while
+% maintaining the original line within resolution meters.
+resolution = 500;
+
+%% AUTOMATED SECTION
+alterParam.properties.resolution = resolution;
+params = read_param_xls(ct_filename_param(paramFn));
+
+fprintf('Altering segments ...\n');
+segments = {};
+season = nan;
+for param_idx = 1:length(params)
+  param = params(param_idx);
+  if param.cmd.generic == 1
+    if ~isempty(regexpi(param.cmd.notes,'do not process'))
+      warning('You have enabled a segment with ''do not process'' in the cmd.notes, dbcont to continue');
+      keyboard
+    end
+    segments{end + 1} = param.day_seg;
+
+    if isnan(season)
+      season = param.season_name;
+    elseif param.season_name ~= season
+      error('Segments are being loaded from multiple seasons. This view only supports altering one season at a time.');
+      keyboard
+    end
+  end
+end
+
+alterParam.properties.segment = segments;
+alterParam.properties.season = season;
+
+%Alter the current segments
+[status,message] = opsAlterPathResolution(sysName,alterParam);
+
+
+clear alterParam params 
