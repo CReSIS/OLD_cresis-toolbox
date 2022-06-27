4d5f78f38297ec6e5a43bd1b434180a34f6a6a77
Reece Mathews <mathewsreece@gmail.com>
2022-06-27 16:27:38 -0500

    Make bulk insert perform the insertion

--- cresis-toolbox/OPS-MATLAB/utility/runOpsBulkInsert.m
+++ cresis-toolbox/OPS-MATLAB/utility/runOpsBulkInsert.m
@@ -42,7 +42,7 @@ global gRadar;
 settings = merge_structs(gRadar,settings);
 
 % RUN OPS BULK INSERT
-% opsBulkInsert(settings);
+opsBulkInsert(settings);
 
 % Perform simplification on the segments
 if PERFORM_SIMPLIFICATION
