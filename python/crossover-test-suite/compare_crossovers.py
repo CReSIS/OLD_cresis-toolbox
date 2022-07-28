358385b39880e091f779e72955e1efd583807693
Reece Mathews <mathewsreece@gmail.com>
2022-07-28 17:41:47 -0500

    Refactor crossover matching

--- python/crossover-test-suite/compare_crossovers.py
+++ python/crossover-test-suite/compare_crossovers.py
@@ -1,10 +1,7 @@
 """Tools for determining the difference between two sets of crossovers."""
-from itertools import groupby, zip_longest
+from itertools import groupby, product, zip_longest
 from json import load
 import math
-
-
-MAX_DISTANCE = 2  # Do not match crossovers more than 2 meters apart
 
 
 def distance(cx_1, cx_2):
@@ -14,50 +11,55 @@ def distance(cx_1, cx_2):
     return cx_1["cx_geom"].distance(cx_2["cx_geom"])
 
 
-def closest_to_point(cx_1, other_crossovers):
-    """Retrieve the closest point to cx_1 from other_crossovers"""
-    distances = [(distance(cx_1, cx_2), cx_2) for cx_2 in other_crossovers]
-    closest = min(distances, key=lambda d: d[0])
-    if closest[0] > MAX_DISTANCE:
-        return None
-    other_crossovers.remove(closest[1])
-    return closest
-
-
 def find_closest(cx_segment_pair_1, cx_segment_pair_2):
-    """Find the closest cx in the other pair to each cx in the first pair."""
+    """Match the crossovers between the pairs to the closest crossover in the other pair."""
+
+    # Add id's to each crossover
+    cx_1 = []
+    for cx_i, cx in enumerate(cx_segment_pair_1):
+        cx_n = cx.copy()
+        cx_n["id"] = cx_i
+        cx_1.append(cx_n)
+    cx_2 = []
+    for cx_i, cx in enumerate(cx_segment_pair_2):
+        cx_n = cx.copy()
+        cx_n["id"] = cx_i
+        cx_2.append(cx_n)
+
+    # Calculate distance between every pair of crossovers
+    distances = []
+    for pair in product(cx_1, cx_2):
+        distances.append((distance(*pair), pair))
+
+    # Iterate distances by lowest and keep first match of each crossover
+    matched_1 = set()
+    matched_2 = set()
     matches = []
-    other_crossovers = cx_segment_pair_2[:]
-
-    no_matches = []
-    last_cx = 0
-    for cx_num, cx_1 in enumerate(cx_segment_pair_1):
-        last_cx = cx_num
-        if not other_crossovers:
-            # More points in pair_1 than pair_2
-            break
+    for pair in sorted(distances, key=lambda d: d[0]):
         
-        closest_point = closest_to_point(cx_1, other_crossovers)
-        if closest_point is None:
-            no_matches.append(cx_1)
+        # Skip pairings involving cx's we've already matched
+        if pair[1][0]["id"] in matched_1:
             continue
+        if pair[1][1]["id"] in matched_2:
+            continue
+        matches.append(pair)
+        matched_1.add(pair[1][0]["id"])
+        matched_2.add(pair[1][1]["id"])
 
-        matches.append((closest_point[0], cx_1, closest_point[1]))
+    # Add any crossovers without matches
+    if len(matched_1) < len(cx_1):
+        for cx in cx_1:
+            if cx["id"] in matched_1:
+                continue
+            matches.append((math.inf, cx, None))
+    if len(matched_2) < len(cx_2):
+        for cx in cx_2:
+            if cx["id"] in matched_2:
+                continue
+            matches.append((math.inf, None, cx))
 
-    if other_crossovers:
-        # More points in pair_2 than pair_1
-        for cx_1, cx_2 in zip_longest(no_matches, other_crossovers, fillvalue=None):
-            if cx_1 is None or cx_2 is None:
-                matches.append((distance(cx_1, cx_2), cx_1, cx_2))
-            else:
-                # TODO[Reece]: Handle extras
-    elif len(matches) < len(cx_segment_pair_1):
-        # More points in pair_1 than pair_2
-        for cx_1 in cx_segment_pair_1[last_cx:]:
-            matches.append((math.inf, cx_1, None))
-
-    print([match[0] for match in matches], len(matches), len(cx_segment_pair_1), len(cx_segment_pair_2))
-    print()
+    return matches
+        
 
 def label_pair(seg1_name, seg2_name):
     """
@@ -94,3 +96,6 @@ if __name__ == "__main__":
     from plot_crossovers import load_data
     data = load_data()
     find_differences(data["0m crossovers"], data["1m crossovers"])
+
+
+# TODO[REECE]: Find crossovers without matches and allow interface to filter for these crossovers
