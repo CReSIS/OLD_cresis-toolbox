7d842efcaed42f2e2ffcc62fdad0b8a20fdfb289
Reece Mathews <mathewsreece@gmail.com>
2022-07-28 16:09:50 -0500

    Reproject to 3413 and add Ruler tool if available

--- python/crossover-test-suite/plot_crossovers.py
+++ python/crossover-test-suite/plot_crossovers.py
@@ -30,7 +30,18 @@ from matplotlib.widgets import Button
 import matplotlib as mpl
 
 from shapely import wkb
+from shapely.ops import transform
+import pyproj
 
+load_ruler = True
+
+try:
+    from matplotlib_tools import Ruler
+except ModuleNotFoundError:
+    load_ruler = False
+    print("Not loading ruler as the matplotlib_tools.py module was not found")
+    print("This file can be obtained from https://github.com/terranjp/matplotlib-tools/blob/master/matplotlib_tools/tools.py")
+    print("I have opted not to include this module in the repo as no license is provided by the author.")
 
 # Default csv field size limit too small for segment geoms
 # Set to max size supported by system
@@ -45,9 +56,21 @@ if Path(os.getcwd()).name == "cresis-toolbox":
 DATA_DIR = Path("data")
 
 
+wgs84 = pyproj.CRS('EPSG:4326')
+greenland = pyproj.CRS('EPSG:3413')
+projection = pyproj.Transformer.from_crs(wgs84, greenland, always_xy=True).transform
+
+
+def reproject(geom_obj):
+    """Convert a geometry to the 3413 CRS."""
+    return transform(projection, geom_obj)
+
+
 def convert_geom(geom: str):
     """Convert geom strs from the Postgis HEXEWKB format to WKB."""
-    return wkb.loads(geom, hex=True)
+    geom_obj = wkb.loads(geom, hex=True)
+    geom_proj = reproject(geom_obj)
+    return geom_proj
 
 
 def load_data():
@@ -71,6 +94,8 @@ def load_data():
 def plot_map():
     """Make a base plot of the Greenland map."""
     map_df = geopandas.read_file("maps/GRL_adm0.shp")
+    map_df = map_df.set_crs("EPSG:4326")
+    map_df = map_df.to_crs("EPSG:3413")
     return map_df.boundary.plot(color='black')
 
 
@@ -117,6 +142,18 @@ def plot_from_data():
 
     data = load_data()
     map_base = plot_map()
+
+    if load_ruler:
+        # Monkey-patch annotate method to handle erroneous use of "s" param instead of "text" by the matplotlib_tools module
+        original_annotate = map_base.annotate
+        map_base.annotate = lambda *args, **kwargs: original_annotate(text=kwargs["s"], *args, **{k: v for k, v in kwargs.items() if k != "s"}) if "s" in kwargs else original_annotate(*args, **kwargs)
+        # Ruler is immediately garbage collected if not assigned to anything
+        # Matplotlib widgets are maintained with only weakrefs internally. The user is expected to maintain their own strong reference
+        ruler = Ruler(ax=map_base, useblit=True, lineprops={"color": "red", "linewidth": "2"})
+
+    map_base.set_xlabel('Meters East of North pole')
+    map_base.set_ylabel('Meters North of North pole')
+
     plot_seg_1m = plot_geoms([row["geom"] for row in data["1m segments"]], map_base, "C0")
     plot_cx_1m = plot_geoms([row["cx_geom"] for row in data["1m crossovers"]], map_base, "C1")
     plot_seg_0m = plot_geoms([row["geom"] for row in data["0m segments"]], map_base, "C2")
@@ -125,8 +162,8 @@ def plot_from_data():
     # Create visibility toggle buttons
     state = State(plot_seg_0m, plot_seg_1m, plot_cx_0m, plot_cx_1m)
 
-    ax_toggle_0 = plt.axes((0.7, 0.05, 0.1, 0.075))
-    ax_toggle_1 = plt.axes((0.81, 0.05, 0.1, 0.075))
+    ax_toggle_0 = plt.axes((0.9, 0.8, 0.1, 0.075))
+    ax_toggle_1 = plt.axes((0.9, 0.7, 0.1, 0.075))
 
     b_toggle_0 = Button(ax_toggle_0, 'Toggle Full Res')
     b_toggle_0.on_clicked(state.toggle_0m)
