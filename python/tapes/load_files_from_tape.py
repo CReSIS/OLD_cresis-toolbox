c0172c9ff1fb7c26cb4424a56a358260813f15a0
Reece Mathews <mathewsreece@ku.edu>
2022-11-15 12:15:02 -0600

    Handle small file archives

--- python/tapes/load_files_from_tape.py
+++ python/tapes/load_files_from_tape.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import re
 import os
 import shutil
+import tarfile
 
-TAPES_FILE = 'tapes.txt'
-OUR_TAPES = 114, 120, 122, 124, 208, 232
+TAPES_FILE = 'tapes.txt'  # Produced from run_get_raw_files.m
 TAPE_LIB_DEV = "/dev/sg4"
 TAPE_MOUNT_PATH = "/mnt/ltfs"
 
@@ -24,6 +24,11 @@ TAPE_MOUNT_PATH = "/mnt/ltfs"
 TAPE_DRIVE_DEVS = {
     "0": "/dev/sg3",
     "1": "/dev/sg5",
+}
+SKIP_EXISTING = True  # Do not ask to overwrite existing files and just skip instead
+FS_PATH_SUBS = {  # Path subsitutions for destinations on the filesystem
+   "/N/dc2/projects/cresis/": "/cresis/snfs1/data/Accum_Data/",
+   "/N/dcwan/projects/cresis/": "/cresis/snfs1/data/Accum_Data/"
 }
 
 
@@ -75,11 +80,17 @@ def parse_tapes_file():
             for tape in tapes:
                 tape_num = get_tape_num(tape)
 
-                if tape_num % 2 == 0:
+                if tape_num % 2 == 0:  # We have even tapes
                     tape_mapping[tape].append(tape_path)
                     break
             else:
                 raise RuntimeError("No even tape for " + tape_path)
+
+    if SKIP_EXISTING:
+        # Remove existing files from list
+        for tape, file_list in tape_mapping.items():
+            files = [file for file in file_list if not os.path.exists(path_subs(path_mapping[file]))]
+            tape_mapping[tape] = files
 
     return tape_mapping, path_mapping
 
@@ -146,7 +157,7 @@ def load_tape(tape_barcode):
 def unmount_drive():
     """Unmount the TAPE_MOUNT_PATH."""
     if os.path.ismount(TAPE_MOUNT_PATH):
-        print("Unmounting /mnt/ltfs")
+        print("Unmounting", TAPE_MOUNT_PATH)
         check_output(["umount", TAPE_MOUNT_PATH])
 
 
@@ -156,12 +167,22 @@ def mount_drive(drive_num):
     unmount_drive()
     drive_dev = TAPE_DRIVE_DEVS[drive_num]
     # TODO[reece]: Handle mount hanging forever
+    print("Mounting " + drive_dev)
     subprocess.call(["ltfs", "-o", f"devname={drive_dev}", TAPE_MOUNT_PATH],
                      stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    if not os.listdir(TAPE_MOUNT_PATH):
+        raise RuntimeError("Failed to mount " + drive_dev)
     try:
         yield
     finally:
         unmount_drive()
+
+
+def path_subs(path):
+    """Perform each path substitution on the given path."""
+    for sub in FS_PATH_SUBS:
+        path = path.replace(sub, FS_PATH_SUBS[sub])
+    return path
 
 
 def copy_files(files, path_mapping):
@@ -173,25 +194,24 @@ def copy_files(files, path_mapping):
         file_path = TAPE_MOUNT_PATH + file
         file_exists = os.path.exists(file_path)
         file_size = os.path.getsize(file_path) / 1024 ** 2 if file_exists else None
-        print(file_exists, f"{file_size}MB", file_path)
+        print("source (exists:)", file_exists, f"{file_size if file_size is not None else 0} MB", file_path)
 
-        fs_file_path = path_mapping[file]
+        fs_file_path = path_subs(path_mapping[file])
         fs_file_exists = os.path.exists(fs_file_path)
         fs_file_size = os.path.getsize(fs_file_path) / 1024 ** 2 if fs_file_exists else None
-        print("->", fs_file_exists, f"{fs_file_size}MB", fs_file_path)
+        print("-> destination (exists:)", fs_file_exists, f"{fs_file_size if fs_file_size is not None else 0} MB", fs_file_path)
 
         # Check if source exists on tape
         if not file_exists:
-            input("**Could not find file on tape**")
+            input("**Could not find source file on tape, perhaps try remounting. Press enter to skip.**")
             continue
 
         # Check if destination already exists on filesystem
         if fs_file_exists:
-            if input("**File already exists on file system**, skip (y) or halt (n)?") == "y":
+            if SKIP_EXISTING or input("**File already exists on file system**, skip (y) or halt (n)?") == "y":
                 continue
             else:
                 raise RuntimeError("File already exists on file system")
-
         # Check if parent folder path exists
         fs_parent_path = Path(fs_file_path).parent
         if not fs_parent_path.exists():
@@ -200,15 +220,34 @@ def copy_files(files, path_mapping):
         # Perform copy
         shutil.copy2(file_path, fs_file_path)
 
+        if fs_file_path.endswith("small_file_archive.tar"):
+            os.chdir(fs_parent_path)
+            with tarfile.open(fs_file_path) as tar:
+                tar.extractall()
+            if os.path.exists("delete_this_zero_file"):
+                os.remove("delete_this_zero_file")
+
+            os.remove(fs_file_path)
+
+        os.chdir("/")
+
 
 def load_tapes(tape_mapping, path_mapping):
     """Load each tape and retrieve the files back to their original location."""
     for tape in tape_mapping:
 
+        if not tape_mapping[tape]:
+            # Skip empty file sets
+            print("Skipping tape with no files: " + tape)
+            continue
+
         try:
             drive_num = load_tape(tape)
         except RuntimeError:
             print("Skipping missing tape: " + tape)
+            print("Did not restore files")
+            for file in tape_mapping[tape]:
+                print("* ", TAPE_MOUNT_PATH + file, "->", path_subs(path_mapping[file]))
             continue
 
         files = tape_mapping[tape]
