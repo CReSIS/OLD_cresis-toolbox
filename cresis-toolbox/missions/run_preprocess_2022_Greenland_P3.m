1c518e103e94992d456cf9d5263b92db5b5cd8f4
cresis1 <cresis_data@ku.edu>
2023-03-02 15:39:04 -0500

    2022_Greenland_P3 updates

--- cresis-toolbox/missions/run_preprocess_2022_Greenland_P3.m
+++ cresis-toolbox/missions/run_preprocess_2022_Greenland_P3.m
@@ -7,7 +7,7 @@ param.config.default = [];
 %% HERC MCORDS6 GROUND
 cur_idx = length(param.config.default)+1;
 param.config.default{cur_idx} = default_radar_params_2022_Greenland_P3_snow();
-param.config.base_dir{cur_idx} = '/mnt/data/';
-param.config.config_folder_names{cur_idx} = '20220419/';
-param.config.board_folder_names{cur_idx} = '20220419/%b';
-param.config.date_strs{cur_idx} = '20220419';
+param.config.base_dir{cur_idx} = '/data/';
+param.config.config_folder_names{cur_idx} = '20220427/';
+param.config.board_folder_names{cur_idx} = '20220427/%b';
+param.config.date_strs{cur_idx} = '20220427';
